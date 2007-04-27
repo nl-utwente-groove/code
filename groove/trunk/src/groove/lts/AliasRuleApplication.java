7582a7cee96041ebd4e62671c94fc5d5e1bb5171
iovka <iovka@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-04-27 13:24:53 +0000

    javadoc

--- groove/trunk/src/groove/lts/AliasRuleApplication.java
+++ groove/trunk/src/groove/lts/AliasRuleApplication.java
@@ -1,16 +1,16 @@
-// GROOVE: GRaphs for Object Oriented VErification
-// Copyright 2003--2007 University of Twente
- 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// http://www.apache.org/licenses/LICENSE-2.0 
- 
-// Unless required by applicable law or agreed to in writing, 
-// software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-// either express or implied. See the License for the specific 
-// language governing permissions and limitations under the License.
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
 /**
  * 
  */
@@ -23,7 +23,7 @@ import groove.trans.RuleApplication;
  * which is a pre-existing graph transition that essentially equals the one we
  * are deriving now, at an earlier stage.
  * @author Arend Rensink
- * @version $Revision: 1.3 $
+ * @version $Revision: 1.4 $
  */
 public interface AliasRuleApplication extends RuleApplication {
     /**
