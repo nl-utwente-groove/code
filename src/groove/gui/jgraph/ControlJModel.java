1c2deddf738a5d5017ba8c18222995a8e067d53d
fladder <fladder@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-06-18 08:36:44 +0000

    implementation of control v0.7

---
+++ src/groove/gui/jgraph/ControlJModel.java
@@ -0,0 +1,18 @@
+package groove.gui.jgraph;
+
+import groove.graph.GraphShape;
+import groove.gui.Options;
+
+public class ControlJModel extends GraphJModel {
+
+	public ControlJModel()
+	{
+		super();
+	}
+	
+	public ControlJModel(GraphShape shape, Options options)
+	{
+		super(shape, options);
+	}
+	
+}
