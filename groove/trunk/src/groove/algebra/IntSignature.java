a980e74c6ccb6952557d92a465520756268f9002
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-07-24 06:16:29 +0000

    Introduced RealAlgebra

--- groove/trunk/src/groove/algebra/IntSignature.java
+++ groove/trunk/src/groove/algebra/IntSignature.java
@@ -3,9 +3,12 @@ package groove.algebra;
 /**
  * Interface for integer algebras. 
  * @author Arend Rensink
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public interface IntSignature<T> {
     /** Addition of two integers. */
     T add(T arg1, T arg2);
+    
+    /** Name of this signature. */
+    static final String NAME = "int";
 }
