d9d06fa4dcc4e315eec563a109606edc07e1ac49
kastenberg <kastenberg@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-07-25 13:56:44 +0000

    licence comments restyled

--- groove/trunk/src/groove/lts/explore/LinearStrategy.java
+++ groove/trunk/src/groove/lts/explore/LinearStrategy.java
@@ -1,18 +1,19 @@
-// GROOVE: GRaphs for Object Oriented VErification
-// Copyright 2003--2007 University of Twente
- 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// http://www.apache.org/licenses/LICENSE-2.0 
- 
-// Unless required by applicable law or agreed to in writing, 
-// software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-// either express or implied. See the License for the specific 
-// language governing permissions and limitations under the License.
-/*
- * $Id: LinearStrategy.java,v 1.2 2007-04-27 22:06:58 rensink Exp $
+/**
+ * GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ *
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ *
+ * $Id: LinearStrategy.java,v 1.3 2007-07-25 13:56:44 kastenberg Exp $
  */
 package groove.lts.explore;
 
@@ -29,7 +30,7 @@ import java.util.Iterator;
  * <i>maximal</i> state is found; that is, a state that only loops back to already explored states.
  * The maximum depth of the search can be set; a depth of 0 means unbounded depth.
  * @author Arend Rensink
- * @version $Revision: 1.2 $
+ * @version $Revision: 1.3 $
  */
 public class LinearStrategy extends AbstractStrategy {
 	/** Name of this exploration strategy. */
