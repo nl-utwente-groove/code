b4d868c315b6ce23901bbf7d79d47671c0a6822a
iovka <iovka@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-11-06 14:07:27 +0000

    comment change

--- groove/trunk/src/groove/io/AbstractXml.java
+++ groove/trunk/src/groove/io/AbstractXml.java
@@ -1,18 +1,19 @@
-// GROOVE: GRaphs for Object Oriented VErification
-// Copyright 2003--2007 University of Twente
- 
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-// http://www.apache.org/licenses/LICENSE-2.0 
- 
-// Unless required by applicable law or agreed to in writing, 
-// software distributed under the License is distributed on an 
-// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
-// either express or implied. See the License for the specific 
-// language governing permissions and limitations under the License.
 /*
- * $Id: AbstractXml.java,v 1.11 2007-10-26 15:37:59 rensink Exp $
+ * GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ 
+ * $Id: AbstractXml.java,v 1.12 2007-11-06 14:07:27 iovka Exp $
  */
 package groove.io;
 
@@ -30,7 +31,7 @@ import java.util.Map;
  * abstract methods: <tt>marshal(Graph)</tt> and <tt>unmarshal(Document,Graph)</tt>.
  * 
  * @author Arend Rensink
- * @version $Revision: 1.11 $
+ * @version $Revision: 1.12 $
  */
 public abstract class AbstractXml implements Xml<Graph> {
 	AbstractXml(GraphFactory graphFactory) {
