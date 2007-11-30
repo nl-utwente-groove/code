bc2733e27d3509e4e544e61112af70c75b007978
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-11-30 08:29:28 +0000

    Refactored to allow other uses than through main

--- groove/trunk/src/groove/util/GraphReporter.java
+++ groove/trunk/src/groove/util/GraphReporter.java
@@ -12,17 +12,19 @@
  * either express or implied. See the License for the specific 
  * language governing permissions and limitations under the License.
  *
- * $Id: GraphReporter.java,v 1.1 2007-11-29 12:51:04 rensink Exp $
+ * $Id: GraphReporter.java,v 1.2 2007-11-30 08:29:28 rensink Exp $
  */
 package groove.util;
 
 import groove.graph.Edge;
 import groove.graph.Graph;
+import groove.graph.GraphShape;
 import groove.graph.Label;
 import groove.view.FormatException;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -37,8 +39,16 @@ public class GraphReporter extends CommandLineTool {
      * Constructs a new graph reporter with a given list of arguments.
      * The arguments consist of a list of options followed by a graph file name.
      */
-    public GraphReporter(List<String> args) {
+    private GraphReporter(List<String> args) {
         super(args);
+    }
+    
+    /** 
+     * Constructs a new reporter, with default settings.
+     * This reporter should exclusively be used to call {@link #getReport(GraphShape)}. 
+     */
+    private GraphReporter() {
+        super(Collections.<String>emptyList());
     }
 
     /** Starts the reporter, for the given list of arguments. */
@@ -83,16 +93,30 @@ public class GraphReporter extends CommandLineTool {
         return super.getUsageMessage()+" graph-file";
     }
 
-    /** Does the actual reporting for a given graph. */
-    private void report(Graph graph) {
+    /** 
+     * Does the actual reporting for a given graph.
+     * The report depends on the parameters of this reporter, and is sent to the standard output. 
+     */
+    public void report(Graph graph) {
+        // count the labels
+        println(getReport(graph).toString());
+    }
+
+    /** 
+     * Generates a report for a given graph.
+     * The report depends on the parameters of this reporter, and is returned in the form of a StringBuilder.
+     */
+    public StringBuilder getReport(GraphShape graph) {
+        StringBuilder result = new StringBuilder();
         // count the labels
         Bag<Label> labels = new TreeBag<Label>();
         for (Edge edge: graph.edgeSet()) {
             labels.add(edge.label());
         }
         for (Map.Entry<Label,? extends Bag.Multiplicity> labelEntry: labels.multiplicityMap().entrySet()) {
-            println(String.format("%s\t%s", labelEntry.getKey(), labelEntry.getValue()));
+            result.append(String.format("%s\t%s%n", labelEntry.getKey(), labelEntry.getValue()));
         }
+        return result;
     }
 
     /**
@@ -100,5 +124,10 @@ public class GraphReporter extends CommandLineTool {
      */
     public static void main(String[] args) {
         new GraphReporter(new LinkedList<String>(Arrays.asList(args))).start();
+    }
+    
+    /** Creates a fresh instance of a reporter. */
+    public static GraphReporter createInstance() {
+        return new GraphReporter();
     }
 }
