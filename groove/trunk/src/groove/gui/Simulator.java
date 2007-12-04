acea58c9fe2d46153d569dcb91e2a6efc17387ba
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2007-12-04 14:30:24 +0000

    Graph grammar loading without start graph repaired

--- groove/trunk/src/groove/gui/Simulator.java
+++ groove/trunk/src/groove/gui/Simulator.java
@@ -12,7 +12,7 @@
  * either express or implied. See the License for the specific 
  * language governing permissions and limitations under the License.
  * 
- * $Id: Simulator.java,v 1.75 2007-12-03 08:52:51 rensink Exp $
+ * $Id: Simulator.java,v 1.76 2007-12-04 14:30:24 rensink Exp $
  */
 package groove.gui;
 
@@ -132,7 +132,7 @@ import javax.swing.table.AbstractTableModel;
 /**
  * Program that applies a production system to an initial graph.
  * @author Arend Rensink
- * @version $Revision: 1.75 $
+ * @version $Revision: 1.76 $
  */
 public class Simulator {
     /**
@@ -682,9 +682,10 @@ public class Simulator {
                     // now we know loading succeeded, we can set the current names & files
                     currentGrammarFile = grammarFile;
                     currentGrammarLoader = grammarLoader;
-                    getStateFileChooser().setCurrentDirectory(currentGrammarFile);
-                    String startFileName = grammar.getStartGraph() == null ? "" : grammar.getStartGraph().getName(); 
-                    getStateFileChooser().setSelectedFile(new File(currentGrammarFile, startFileName));
+                    if (grammar.getStartGraph() != null) {
+                        File startFile = new File(grammarFile, grammar.getStartGraph().getName());
+                        getStateFileChooser().setSelectedFile(startFile);
+                    }
                     getGrammarFileChooser().setSelectedFile(grammarFile);
                 } catch (final IOException exc) {
                     SwingUtilities.invokeLater(new Runnable() {
