62c4d199eb079bce22ba1b16cdea7180249b8e87
fladder <fladder@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-01-30 11:25:25 +0000

    removal of unused classes in control package

--- groove/trunk/src/groove/control/StateSet.java
+++
@@ -1,22 +0,0 @@
-/* GROOVE: GRaphs for Object Oriented VErification
- * Copyright 2003--2007 University of Twente
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific 
- * language governing permissions and limitations under the License.
- *
- * $Id: StateSet.java,v 1.6 2008-01-30 11:13:57 fladder Exp $
- */
-package groove.control;
-import groove.trans.Rule;import java.util.HashSet;import java.util.Map;import java.util.Set;import java.util.SortedMap;import java.util.TreeMap;import java.util.TreeSet;
-/** *  * @author Tom Staijen * @version $Revision $ */public class StateSet extends HashSet<ControlState> {	/**	 * member variable for caching the result of {@link #getRuleSets()}.	 */	Set<Rule> rules;
-
-//	/**//	 * Returns a prioritized Set of Sets of Rules.//	 * This is done by merging the prioritized rulesets//	 * of all ControlStates and then by appending the prioritized//	 * rulesets of the Else-Targets //	 * //	 * @return Set<Rule>//	 *///	public Set<Rule> rules() {//		// for now, priorities will be ignores when using control ?//		if( this.rules == null ) {//			this.rules = new HashSet<Rule>();//			for( ControlState cs : this ) {//				this.rules.addAll(cs.rules());//			}//		}//		return rules;//	}
-	/**	 * Default Constructor	 */	public StateSet() {		super();	}}
