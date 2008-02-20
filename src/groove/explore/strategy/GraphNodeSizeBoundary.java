2e8976902bd5aa44fb7a013ec060a484caaa3b05
kastenberg <kastenberg@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-02-20 08:37:12 +0000

    *** empty log message ***

---
+++ src/groove/explore/strategy/GraphNodeSizeBoundary.java
@@ -0,0 +1,50 @@
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ *
+ * $Id: GraphNodeSizeBoundary.java,v 1.1 2008-02-20 08:37:12 kastenberg Exp $
+ */
+package groove.explore.strategy;
+
+import groove.lts.GraphTransition;
+
+/**
+ * Implementation of interface {@link Boundary} that
+ * bases the boundary on the node-count of the graph reached
+ * by the given transition.
+ * 
+ * @author Harmen Kastenberg
+ * @version $Revision: 1.1 $ $Date: 2008-02-20 08:37:12 $
+ */
+public class GraphNodeSizeBoundary implements Boundary {
+
+	/**
+	 * {@link GraphNodeSizeBoundary} constructor.
+	 * @param graphSizeBoundary value that defined the boundary
+	 */
+	public GraphNodeSizeBoundary(int graphSizeBoundary, int step) {
+		this.graphSizeBoundary = graphSizeBoundary;
+		this.step = step;
+	}
+
+	public boolean crossingBoundary(GraphTransition transition) {
+		return transition.target().getGraph().nodeCount() > graphSizeBoundary;
+	}
+
+	public void increase() {
+		this.graphSizeBoundary += this.step;
+	}
+
+	private int graphSizeBoundary;
+	private int step;
+}
