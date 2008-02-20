9357715790180bacfa081c92cd9755cdc4110cb9
kastenberg <kastenberg@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-02-20 08:36:06 +0000

    initial version

---
+++ src/groove/explore/strategy/Boundary.java
@@ -0,0 +1,45 @@
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ *
+ * $Id: Boundary.java,v 1.1 2008-02-20 08:35:19 kastenberg Exp $
+ */
+package groove.explore.strategy;
+
+import groove.lts.GraphTransition;
+
+/**
+ * This interface facilitates a model checking strategy
+ * of using a boundary such that states reached by transitions
+ * crossing the boundary will not be taken into account (yet).
+ * 
+ * @author Harmen Kastenberg
+ * @version $Revision: 1.1 $ $Date: 2008-02-20 08:35:19 $
+ */
+public interface Boundary {
+
+	/**
+	 * Checks whether the given transition crosses the boundary.
+	 * If so, it return <tt>true</tt>, otherwise <tt>false</tt>.
+	 * @param transition the transition for which to check whether
+	 * it crosses the boundary
+	 * @return <tt>true</tt> if the transition crosses the boundary,
+	 * <tt>false</tt> otherwise
+	 */
+	public boolean crossingBoundary(GraphTransition transition);
+
+	/**
+	 * Increases the boundary.
+	 */
+	public void increase();
+}
---
+++ src/groove/explore/strategy/RuleSetBoundary.java
@@ -0,0 +1,61 @@
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ *
+ * $Id: RuleSetBoundary.java,v 1.1 2008-02-20 08:36:06 kastenberg Exp $
+ */
+package groove.explore.strategy;
+
+import groove.lts.GraphTransition;
+import groove.trans.Rule;
+
+import java.util.Set;
+
+/**
+ * Implementation of interface {@link Boundary} that
+ * bases the boundary on a set of rules for which application
+ * are said to cross the boundary. When increasing the boundary
+ * this class allows the exploration of a single rule that
+ * would otherwise not be allowed. Thereafter, the same boundary
+ * is applied again.
+ * 
+ * @author Harmen Kastenberg
+ * $Id: RuleSetBoundary.java,v 1.1 2008-02-20 08:36:06 kastenberg Exp $
+ */
+public class RuleSetBoundary implements Boundary {
+
+	/**
+	 * {@link RuleSetBoundary} constructor.
+	 * @param ruleSetBoundary the set of rules that constitute the boundary
+	 */
+	public RuleSetBoundary(Set<Rule> ruleSetBoundary) {
+	}
+
+	public boolean crossingBoundary(GraphTransition transition) {
+		boolean crossingBoundary = ruleSetBoundary.contains(transition.getEvent().getRule()); 
+		if (crossingBoundary) {
+			if (allowBoundaryRule) {
+				allowBoundaryRule = false;
+				return false;
+			}
+		}
+		return false;
+	}
+
+	public void increase() {
+		allowBoundaryRule = true;
+	}
+
+	private Set<Rule> ruleSetBoundary;
+	private boolean allowBoundaryRule;
+}
