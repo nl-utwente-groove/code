57806212c4ce5270cfb8c8fde239f05968800716
kastenberg <kastenberg@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-02-20 10:01:39 +0000

    adding listeners to the GTS

--- groove/trunk/src/groove/explore/DefaultScenario.java
+++ groove/trunk/src/groove/explore/DefaultScenario.java
@@ -1,3 +1,20 @@
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2007 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an 
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific 
+ * language governing permissions and limitations under the License.
+ *
+ * $Id: DefaultScenario.java,v 1.6 2008-02-20 10:01:39 kastenberg Exp $
+ */
+
 package groove.explore;
 
 import groove.explore.result.Acceptor;
@@ -96,8 +113,9 @@ public class DefaultScenario<T> implements Scenario<T> {
 		// make sure strategy and acceptor are reset and up to date
 		strategy.setState(atState);
 		strategy.setGTS(gts);
-		
+
 		gts.addGraphListener(acceptor);
+		strategy.addGTSListener(acceptor);
 		
 		acceptor.setResult(prototype);
 
