d55e3d90d65f85aa10c666de33abb7c70a41632f
kastenberg <kastenberg@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-04-07 13:53:24 +0000

    correctly set next start-state (initialization)

--- src/groove/explore/strategy/OptimizedBoundedNestedDFSStrategy.java
+++ src/groove/explore/strategy/OptimizedBoundedNestedDFSStrategy.java
@@ -62,8 +62,12 @@ public class OptimizedBoundedNestedDFSStrategy extends BoundedNestedDFSStrategy 
 					if (getBoundary() instanceof GraphNodeSizeBoundary && ((GraphNodeSizeBoundary) getBoundary()).crossingBoundary(nextOpenState.getGraph())) {
 						continue;
 					} else {
-						this.atBuchiState = nextOpenState;
+						setAtBuchiState(nextOpenState);
+//						this.atBuchiState = nextOpenState;
 						ModelChecking.nextColourScheme();
+						searchStack().clear();
+						transitionStack().clear();
+						lastTransition = null;
 					}
 				}
 			}
