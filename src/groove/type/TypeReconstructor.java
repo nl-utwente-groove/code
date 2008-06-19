f0e90c65a9e9ab6f6c59f59b197a807c156c24ea
fjvanes <fjvanes@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2008-06-19 15:31:49 +0000

    Updated version of the Type Inference algorithm. Plus: it does not compute the type graph when starting the simulator, but only after pushing the "create type graph" button on the type panel.

--- src/groove/type/TypeReconstructor.java
+++ src/groove/type/TypeReconstructor.java
@@ -16,15 +16,15 @@
  */
 package groove.type;
 
-import groove.graph.AbstractNodeEdgeMap;
 import groove.graph.DefaultEdge;
 import groove.graph.DefaultGraph;
 import groove.graph.DefaultNode;
 import groove.graph.Edge;
-import groove.graph.GenericNodeEdgeHashMap;
 import groove.graph.Graph;
 import groove.graph.MergeMap;
 import groove.graph.Node;
+import groove.graph.NodeEdgeHashMap;
+import groove.graph.NodeEdgeMap;
 import groove.trans.GraphGrammar;
 import groove.trans.Rule;
 import groove.trans.RuleMatch;
@@ -33,31 +33,27 @@ import groove.view.FormatException;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * @author Frank van Es
  * @version $Revision $
  */
 public class TypeReconstructor {
+	
+	private Map<Graph,NodeEdgeMap> typings = new HashMap<Graph,NodeEdgeMap>();
+	private Graph typeGraph = new DefaultGraph();
+	private MergeMap nodeTypes = new MergeMap();
+	
+	
 	/**
-	 * Reconstructs a type graph for a graph grammar
-	 * @param grammar A graph grammar to construct a type graph for
-	 * @return A type graph for grammar 
+	 * @param grammar
 	 */
-	public static Graph reconstruct(GraphGrammar grammar) {
-		
+	protected TypeReconstructor(GraphGrammar grammar) {
 		Graph startGraph = grammar.getStartGraph();
 		Collection<Rule> rules = grammar.getRules();
 		
-		Map<Rule,AbstractNodeEdgeMap<Node,Node,Edge,Edge>> ruleMappings = 
-			new HashMap<Rule,AbstractNodeEdgeMap<Node,Node,Edge,Edge>>();
-		
-		Graph typeGraph = new DefaultTypeGraph();
-		
-		addTyping(typeGraph, startGraph);
+		addTyping(startGraph);
 		
 		boolean addedRule;
 		int nodeCount;
@@ -70,101 +66,122 @@ public class TypeReconstructor {
 				
 				if (removeApplicationConditions(rule).hasMatch(typeGraph)) {
 					
-					if (ruleMappings.get(rule) == null) {
+					if (getTyping(rule.rhs()) == null) {
 						addedRule = true;
-						ruleMappings.put(rule, addTyping(typeGraph, rule));
-						//ruleMappings.put(rule, addTyping(typeGraph, rule.lhs()));
+						addTyping(rule.lhs());
+						addTyping(rule.rhs());
 					}
 					
-					AbstractNodeEdgeMap<Node,Node,Edge,Edge> map = ruleMappings.get(rule);
-					
 					// TODO: Possibility: updated putAll method in MergeMap class
-					MergeMap newMerges = calculateMerges(typeGraph, rule, map);
+					MergeMap newMerges = calculateMerges(rule);
 					for (Map.Entry<Node,Node> merge : newMerges.nodeMap().entrySet()) {
 						equivalentTypes.putNode(merge.getKey(), merge.getValue());
 					}
 				}
 			}
 			for (Map.Entry<Node,Node> mapping : equivalentTypes.nodeMap().entrySet()) {
-				typeGraph.mergeNodes(mapping.getKey(), mapping.getValue());
+				Node from = getNodeType(mapping.getKey());
+				Node to = getNodeType(mapping.getValue());
+				if (from != null && to != null) {
+					typeGraph.mergeNodes(from, to);
+					nodeTypes.putNode(from, to);
+				}
 			}
 		} while (addedRule || typeGraph.nodeCount() != nodeCount);
-		
+	}
+	
+	/**
+	 * @return
+	 */
+	public Graph getTypeGraph() {
 		return typeGraph;
 	}
 	
 	/**
+	 * Reconstructs a type graph for a graph grammar
+	 * @param grammar A graph grammar to construct a type graph for
+	 * @return A type graph for grammar 
+	 */
+	public static Graph reconstruct(GraphGrammar grammar) {
+		
+		return new TypeReconstructor(grammar).getTypeGraph();
+	}
+	
+	/**
 	 * Calculates which nodes in the type graph need to be merged based on the
-	 * given production rule and current typing morphisms into the type graph.
-	 * @param typeGraph The type graph in which the rule should be matched
-	 * @param rule The rule to compute matchies into the type graph from
-	 * @param currentTypings Current typings, which state which elements of the
-	 *        rule graphs are represented by which elements in the type graph
+	 * given production rule.
+	 * @param rule The rule to compute matches into the type graph from
 	 * @return A MergeMap containing mappings between nodes that need to be merged 
 	 */
-	public static MergeMap calculateMerges(Graph typeGraph, 
-			Rule rule, AbstractNodeEdgeMap<Node,Node,Edge,Edge> currentTypings) {
+	private MergeMap calculateMerges(Rule rule) {
+		
+		NodeEdgeMap lhsTyping = getTyping(rule.lhs());
+		NodeEdgeMap rhsTyping = getTyping(rule.rhs());
+		NodeEdgeMap newLhsTyping = getTyping(rule.lhs());
 		
 		MergeMap merges = new MergeMap();
+		Map<Node,Node> nodeMap;
+		
+		nodeMap = rule.getMorphism().elementMap().nodeMap();
+		for (Map.Entry<Node,Node> nodes : nodeMap.entrySet()) {
+			merges.putNode(
+					lhsTyping.getNode(nodes.getKey()), 
+					rhsTyping.getNode(nodes.getValue())
+			);
+		}
 		
 		// Create a copy of the current rule in order to omit application
 		// conditions and compute the matches from this rule into the 
 		// current type graph
 		Iterable<RuleMatch> matches = removeApplicationConditions(rule).getMatches(typeGraph, null);
 		for (RuleMatch match : matches) {
-			Map<Node,Node> nodeMap = match.getElementMap().nodeMap();
+			nodeMap = match.getElementMap().nodeMap();
 			for (Map.Entry<Node,Node> nodes : nodeMap.entrySet()) {
 				merges.putNode(
-						currentTypings.getNode(nodes.getKey()), 
+						lhsTyping.getNode(nodes.getKey()), 
 						nodes.getValue()
 				);
 			}
 		}
 		
+		typings.put(rule.lhs(), newLhsTyping);
 		
 		return merges;
 	}
 	
 	/**
-	 * @param typeGraph
 	 * @param graph
-	 * @return
 	 */
-	public static AbstractNodeEdgeMap<Node,Node,Edge,Edge> addTyping(Graph typeGraph, Graph graph) {
+	private void addTyping(Graph graph) {
 		
-		AbstractNodeEdgeMap<Node,Node,Edge,Edge> result = 
-			new GenericNodeEdgeHashMap<Node,Node,Edge,Edge>();
+		NodeEdgeMap typing = new NodeEdgeHashMap();
 		
 		for (Node node : graph.nodeSet()) {
 			Node newNode = DefaultNode.createNode();
-			result.putNode(node, newNode);
+			typing.putNode(node, newNode);
 			typeGraph.addNode(newNode);
 		}
 		for (Edge edge : graph.edgeSet()) {
 			Edge newEdge = DefaultEdge.createEdge(
-					result.getNode(edge.end(Edge.SOURCE_INDEX)),
+					typing.getNode(edge.end(Edge.SOURCE_INDEX)),
 					edge.label(),
-					result.getNode(edge.end(Edge.TARGET_INDEX))
+					typing.getNode(edge.end(Edge.TARGET_INDEX))
 			);
-			result.putEdge(edge, newEdge);
+			typing.putEdge(edge, newEdge);
 			typeGraph.addEdge(newEdge);
 		}
 		
-		return result;
+		typings.put(graph, typing);
 	}
 	
-	public static AbstractNodeEdgeMap<Node,Node,Edge,Edge> addTyping(Graph typeGraph, Rule rule) {
-		
-		Graph resultGraph = new DefaultGraph();
-		resultGraph.addNodeSet(rule.rhs().nodeSet());
-		resultGraph.addEdgeSet(rule.rhs().edgeSet());
-		resultGraph.addNodeSet(rule.lhs().nodeSet());
-		resultGraph.addEdgeSet(rule.lhs().edgeSet());
-		
-		return addTyping(typeGraph, resultGraph);
+	private NodeEdgeMap getTyping(Graph graph) {
+		return typings.get(graph);
 	}
 	
-	
+	/**
+	 * @param rule The rule of which the application conditions need to be removed
+	 * @return A copy of rule with all application conditions removed
+	 */
 	public static Rule removeApplicationConditions(Rule rule) {
 		Rule result = null;
 		try {
@@ -182,4 +199,14 @@ public class TypeReconstructor {
 		return result;
 	}
 	
+	private Node getNodeType(Node node) {
+		if (typeGraph.containsElement(node)) {
+			return node;
+		} else if (typeGraph.containsElement(nodeTypes.getNode(node))) {
+			return nodeTypes.getNode(node);
+		} else {
+			//System.err.println("Node not found! " + node.getClass().getCanonicalName());
+			return null;
+		}
+	}
 }
