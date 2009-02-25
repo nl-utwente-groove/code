40588f1b859e3a12e6925422c5042496f5d92e94
zambon <zambon@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2009-02-25 15:13:15 +0000

    Finished feature request for Start graphs list.

--- src/groove/gui/StateJList.java
+++ src/groove/gui/StateJList.java
@@ -21,15 +21,19 @@ import groove.lts.GraphState;
 import groove.lts.GraphTransition;
 import groove.trans.NameLabel;
 import groove.trans.RuleMatch;
-import groove.view.AspectualGraphView;
 import groove.view.DefaultGrammarView;
 
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.File;
+import java.util.Arrays;
 
 import javax.swing.JList;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.ListSelectionModel;
 
 /**
@@ -47,38 +51,64 @@ public class StateJList extends JList implements SimulationListener {
         this.simulator.addSimulationListener(this);
         this.setEnabled(false);
         this.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-
-        MouseListener mouseListener = new MouseAdapter() {
-            /** Update the state when a list element is double-clicked */
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                if (e.getClickCount() == 2) {
-                    String selection =
-                        (String) StateJList.this.getSelectedValue();
-                    File file =
-                        StateJList.this.simulator.getCurrentGrammar().getGraphs().get(
-                            selection);
-                    StateJList.this.simulator.doLoadStartGraph(file);
-                    StateJList.this.simulator.getStateFileChooser().setSelectedFile(
-                        file);
-                } else if (e.getClickCount() == 1) {
-                    try {
-                        wait(100);
-                    } catch (Exception ex) {
-                        // riiiiiiight...
-                    }
-                    AspectualGraphView start =
-                        StateJList.this.simulator.getCurrentGrammar().getStartGraph();
-                    if (start != null) {
-                        StateJList.this.setSelectedValue(start.getName(), false);
-                    }
-                }
+        this.popupMenu = createPopupMenu();
+        this.addMouseListener(new MyMouseListener());
+    }
+    
+    /**
+     * In addition to delegating the method to <tt>super</tt>, sets the
+     * background color to <tt>null</tt> when disabled and back to the default
+     * when enabled.
+     */
+    @Override
+    public void setEnabled(boolean enabled) {
+        if (enabled != isEnabled()) {
+            if (!enabled) {
+                this.enabledBackground = getBackground();
+                setBackground(null);
+            } else if (this.enabledBackground != null) {
+                setBackground(this.enabledBackground);
             }
-        };
-        this.addMouseListener(mouseListener);
-
+        }
+        super.setEnabled(enabled);
     }
 
+    /**
+     * Creates a popup menu, consisting of "use as start graph" & "preview".
+     */
+    protected JPopupMenu createPopupMenu() {
+        JPopupMenu result = new JPopupMenu();
+        JMenuItem item;
+        result.add(item = new JMenuItem(Options.START_GRAPH_ACTION_NAME));
+        item.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                String selection = (String)StateJList.this.getSelectedValue();
+                File file = StateJList.this.simulator.getCurrentGrammar().getGraphs().get(selection);
+                StateJList.this.simulator.doLoadStartGraph(file);
+              }
+            });
+        result.add(item = new JMenuItem(Options.PREVIEW_ACTION_NAME));
+        item.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                StateJList.this.doPreviewGraph();
+              }
+            });
+        return result;
+    }
+    
+    private void doPreviewGraph() {
+        String selection = (String)this.getSelectedValue();
+        File file = this.simulator.getCurrentGrammar().getGraphs().get(selection);
+        boolean load = Editor.previewGraph(file, Options.START_GRAPH_ACTION_NAME);
+        if (load) {
+            this.simulator.doLoadStartGraph(file);
+        }
+    }
+
+    // -----------------------------------------
+    // Methods from SimulationListener Interface
+    // -----------------------------------------
+    
     public void applyTransitionUpdate(GraphTransition transition) {
         // TODO Auto-generated method stub
     }
@@ -88,7 +118,9 @@ public class StateJList extends JList implements SimulationListener {
         this.setEnabled(false);
 
         if (grammar != null) {
-            this.setListData(grammar.getGraphs().keySet().toArray());
+            Object grammarNames[] = grammar.getGraphs().keySet().toArray();
+            Arrays.sort(grammarNames);
+            this.setListData(grammarNames);
             if (grammar.getStartGraph() != null) {
                 String startGraph = grammar.getStartGraph().getName();
                 this.setSelectedValue(startGraph, true);
@@ -122,10 +154,53 @@ public class StateJList extends JList implements SimulationListener {
 
     }
 
+    // --------------
+    // Private fields
+    // --------------
+    
     /**
      * The simulator to which this directory belongs.
      * @invariant simulator != null
      */
     protected final Simulator simulator;
+
+    /**
+     * The background colour of this component when it is enabled.
+     */
+    private Color enabledBackground;
+    
+    /**
+     * The pop-up menu for this states list.
+     * @invariant popupMenu != null
+     */
+    protected final JPopupMenu popupMenu;
+
+    // -----------------------------------------
+    // MyMouseListener Class
+    // -----------------------------------------
+    
+    /** Class to deal with mouse events over the label list. */
+    private class MyMouseListener extends MouseAdapter {
+        
+        /** Empty constructor with the correct visibility. */
+        MyMouseListener() {
+            // empty
+        }
+        
+        @Override
+        public void mouseClicked(MouseEvent evt) {
+            int index = locationToIndex(evt.getPoint());
+            if (StateJList.this.isEnabled() && index >= 0) {
+                if (evt.getClickCount() == 2) { // Left double click
+                    StateJList.this.doPreviewGraph();
+                } else if (evt.getClickCount() == 1 &&
+                           evt.getButton() == MouseEvent.BUTTON3) { // Right click
+                    // Adjust list selection accordingly.
+                    StateJList.this.setSelectedIndex(index);
+                    StateJList.this.popupMenu.show(evt.getComponent(), evt.getX(), evt.getY());
+                }
+            }
+        }
+    }
 
 }
