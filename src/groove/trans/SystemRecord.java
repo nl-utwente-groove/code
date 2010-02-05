926c417c551715a50ac4f40b22714e207fab6efe
keijsers <keijsers@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2010-02-05 06:41:40 +0000

    Should use the old implementation again (LocationCache instead of ControlStateCache).

--- src/groove/trans/SystemRecord.java
+++ src/groove/trans/SystemRecord.java
@@ -318,7 +318,7 @@ public class SystemRecord implements NodeFactory {
             result =
                 new PriorityCache(this.ruleSystem.getRuleMap(), isRandomized);
         } else if (state.getLocation() != null) {
-            if (true) {
+            if (false) {
                 result =
                     new LocationCache((ControlLocation) state.getLocation(),
                         state, isRandomized);
