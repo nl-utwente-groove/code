23977433f8cf4abfa570f9e25471eb06566dd777
zambon <zambon@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2011-02-23 10:18:42 +0000

    Tagged points of improvement in the abstraction code.

--- src/groove/abstraction/CountingSet.java
+++ src/groove/abstraction/CountingSet.java
@@ -23,6 +23,9 @@ import java.util.HashMap;
  * been inserted in the set.
  * 
  * @author Eduardo Zambon
+ * 
+ * EDUARDO: Pimp this.
+ * Use a better data structure...
  */
 public final class CountingSet<T> extends HashMap<T,Integer> {
 
