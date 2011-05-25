3f1a5c88af603894a185ee4f2ad7f08eac49e771
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2011-05-25 06:50:41 +0000

    Bug repaired in switching to erroneous view

--- groove/trunk/src/groove/graph/TypeGraph.java
+++ groove/trunk/src/groove/graph/TypeGraph.java
@@ -102,6 +102,7 @@ public class TypeGraph extends NodeSetEdgeSetGraph<TypeNode,TypeEdge> {
             if (node.isImported()) {
                 this.imports.add(node);
             }
+            this.labelStore.addLabel(node.getType());
         }
         return result;
     }
