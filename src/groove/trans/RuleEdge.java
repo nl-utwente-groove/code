22fdf709d02ba5333f3e708d0dda0ca370bfb894
Arend Rensink <arend.rensink@utwente.nl>
2011-12-31 12:58:22 +0000

    Improved regular expression type checking

--- src/groove/trans/RuleEdge.java
+++ src/groove/trans/RuleEdge.java
@@ -18,8 +18,12 @@ package groove.trans;
 
 import groove.graph.AbstractEdge;
 import groove.graph.TypeEdge;
+import groove.graph.TypeElement;
 import groove.graph.algebra.ArgumentEdge;
 import groove.graph.algebra.OperatorEdge;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Supertype of all edges that may appear in a {@link RuleGraph}.
@@ -47,6 +51,22 @@ public class RuleEdge extends AbstractEdge<RuleNode,RuleLabel> implements
         return this.type;
     }
 
+    /**
+     * Returns the optional set of possible edge types,
+     * if the label of this edge is a wildcard.
+     */
+    public Set<TypeElement> getWildcardTypes() {
+        return this.wildcardTypes;
+    }
+
+    /** Sets the possible types of a wildcard edge. */
+    public void setWildcardTypes(Set<? extends TypeElement> wildcardTypes) {
+        assert this.label.isWildcard();
+        this.wildcardTypes = new HashSet<TypeElement>(wildcardTypes);
+    }
+
     /** The edge type of this rule edge. */
     private final TypeEdge type;
+    /** Set of possible edge types, if the label is a wildcard. */
+    private Set<TypeElement> wildcardTypes;
 }
