e3e9007ef78a8b8f6618757acc4249b1794f46e9
vincentdebruijn <vincentdebruijn@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2012-01-30 17:03:01 +0000

    Initiated changes on RemoteStrategy to allow proper transformation to an STS

--- src/groove/explore/strategy/RemoteStrategy.java
+++ src/groove/explore/strategy/RemoteStrategy.java
@@ -19,12 +19,22 @@ package groove.explore.strategy;
 import groove.lts.GTS;
 import groove.lts.GraphState;
 import groove.lts.GraphTransition;
+import groove.lts.MatchResult;
+import groove.lts.MatchResultSet;
 import groove.lts.RuleTransition;
+import groove.trans.Condition;
+import groove.trans.HostEdge;
+import groove.trans.HostGraph;
+import groove.trans.HostNode;
+import groove.trans.Rule;
+import groove.trans.RuleGraph;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -58,22 +68,95 @@ public class RemoteStrategy extends AbstractStrategy {
 	@Override
 	public void prepare(GTS gts, GraphState startState) {
 		super.prepare(gts, startState);
+		
 		// Initiate the Depth-First strategy
 		this.dfsStrategy = new DFSStrategy();
 		this.dfsStrategy.prepare(gts, startState);
+		
+		//connect();
+		this.sts = new STS();
+		this.sts.toLocation(this.sts.hostGraphToLocation(startState.getGraph()));
 	}
+	
+	@Override
+	public boolean next() {
+		if (getState() == null) {
+			return false;
+	    }
+		
+		// If the location is new, determine its outgoing switch relations
+		Location current = this.sts.getCurrentLocation();
+		if (!current.explored()) {
+			// Get current rule matches
+			Set<Rule> rules = new HashSet<Rule>();
+			for (MatchResult next : createMatchCollector().getMatchSet()) {
+            	RuleTransition transition = getMatchApplier().apply(getState(), next);
+				Rule rule = next.getEvent().getRule();
+				if (!rules.contains(rule)) {
+					SwitchRelation sr = this.sts.ruleToSwitchRelation(rule);
+	            	Location l = this.sts.hostGraphToLocation(transition.target().getGraph());
+					current.addSwitchRelation(sr, l);
+					rules.add(rule);
+				}
+	        }
+		}
+        return updateAtState();
+    }
 
+	/**
+	 * Set the remote host.
+	 * @param host The remote host
+	 */
 	public void setHost(String host) {
 		this.host = host;
+	}
+	
+	public void setUseDFS(boolean use) {
+		this.useDFS = use;
 	}
 
 	@Override
 	protected GraphState getNextState() {
-		GraphState result = dfsStrategy.getNextState();
+		GraphState result = getRemoteStrategy();
 		if (result == null) {
-			send(format_to_simple_sts(getGTS()));
+			//send(format_to_simple_sts(getGTS()));
+			send(this.sts.toJSON());
+			disconnect();
 		}
-		return result;
+		return null;
+		//return result;
+	}
+	
+	// Sends the possible rule matches to the remote server.
+	// Wait for rule choice. Apply the chosen rule.
+	private GraphState getRemoteStrategy() {
+		Location current = this.sts.getCurrentLocation();
+		
+		if (useDFS) {
+			// Use the DfsStrategy to decide on the next state.
+			GraphState state = dfsStrategy.getNextState();
+			this.sts.toLocation(this.sts.getLocation(state.getGraph()));
+			return state;
+		} else {
+			// Send the possible switch relations from the current location to the remote server.
+			Set<SwitchRelation> relations = current.getSwitchRelations();
+			send(createMessage(relations));
+			
+			// Get the response with the choice from the remote server.
+			InstantiatedSwitchRelation isr = receive();
+			this.sts.toLocation(current.getRelationTarget(isr.getSwitchRelation()));
+			
+			return isr.getTransition().target();
+		}
+	}
+	
+	private String createMessage(Set<SwitchRelation> relations) {
+		String message = "[";
+		for (SwitchRelation r : relations ) {
+			message += r.toJSON()+",";
+		}
+		message = message.substring(0, message.length()-1)+"]";
+ 		return message;
 	}
 
   // Transforms the gts to a simple sts.
@@ -151,67 +234,6 @@ public class RemoteStrategy extends AbstractStrategy {
 		return message;
 	}
 
-  // Transforms the gts to an sts.
-  // TODO: create transformation rules
-  // @return: a JSON formatted string containing the sts
-	private String format_to_sts(GTS gts) {
-		String message = "[";
-		// For each state
-		Iterator<? extends GraphState> nodesIter = gts.nodeSet().iterator();
-		while(nodesIter.hasNext()) {
-			GraphState node = nodesIter.next();
-			Map<String, List<Object[]>> valueMap = new HashMap<String, List<Object[]>>(); // maps a label to a list of parameter values
-			Set<String> noParamEdges = new HashSet<String>(); // if a label has no parameter edges, future edges with that label will also not have parameters
-			// For each outgoing edge
-			Iterator<RuleTransition> edgeIter = node.getTransitionIter();
-			while(edgeIter.hasNext()) {
-				GraphTransition edge = edgeIter.next();
-				String label = edge.label().text();
-				if(!noParamEdges.contains(label)) {
-					// Find if the label has parameters
-					Pattern pattern = Pattern.compile("\\(.+\\)$");
-					Matcher matcher = pattern.matcher(edge.label().text());
-					if(matcher.find()) {
-						String paramString = label.substring(matcher.start());
-						label = label.substring(0, matcher.start());
-						// Parse the parameters
-						Object[] params = parseParams(paramString);
-						// Map the label to these parameter values
-						List<Object[]> values = valueMap.get(label);
-						if(values == null) {
-							values = new ArrayList<Object[]>();
-							valueMap.put(label, values);
-						}
-						values.add(params);
-					} else {
-						// Remove possible empty parameter
-						//if(label.endsWith("()"))
-						//label = label.substring(0,label.length()-2);
-						// We do not have to explore future edges with this label
-						noParamEdges.add(label);
-					}
-				}
-			}
-			// For each label
-			Iterator<String> ix = valueMap.keySet().iterator();
-			while (ix.hasNext()) {
-				String label = ix.next();
-				List<Object[]> params = valueMap.get(label);
-				// Construct the constraint (guard) by doing some smart partitioning
-				//String guard = partitionParams(params);
-				String type = null;
-				if (label.contains("?"))
-					type = "stimulus";
-				else
-					type = "response";
-				// TODO: fix this problem
-				//message += "[\"s"+node.getNumber()+"\",[\""+label+"\",\""+type+"\"],\"s"+edge.target().getNumber()+"\"],";
-			}
-		}
-		message = message.substring(0, message.length()-1)+"]";
-		return message;
-	}
-
 	// Parses a "(x, y, ...)" string to an array of Objects representing x, y, etc.
 	private Object[] parseParams(String paramString) {
 		String[] split = paramString.substring(1,paramString.length()-1).split(",");
@@ -276,48 +298,222 @@ public class RemoteStrategy extends AbstractStrategy {
 		}
 	}
 
-	// Sends a JSON message to the remote server
-	private void send(String message) {
-		// Connect to the remote server
+	// Connect to the remote server
+	private void connect() {
 		try {
 			// Create a URLConnection object for a URL
 			URL url = new URL(host);
-			HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+			this.conn = (HttpURLConnection)url.openConnection();
 			conn.setDoOutput(true);
 			conn.setRequestMethod("POST");
 			conn.setReadTimeout(10000);
 			conn.setRequestProperty("Content-Type","application/json");
-
-			System.out.println(message);
-
+	
 			conn.connect();
-
-			OutputStreamWriter out = new OutputStreamWriter(
-					conn.getOutputStream());
-			out.write(message);
-			out.close();
-
-			BufferedReader in = new BufferedReader(
-					new InputStreamReader(
-							conn.getInputStream()));
-
-			String decodedString;
-			System.out.println("Server response:");
-
-			while ((decodedString = in.readLine()) != null) {
-				System.out.println(decodedString);
-			}
-			in.close();
-
-			conn.disconnect();
-
+	
+			this.out = new OutputStreamWriter(conn.getOutputStream());
+			this.in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	// Disconnect from the remote server.
+	private void disconnect() {
+		try {
+			in.close();
+			out.close();
+			conn.disconnect();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	// Sends a JSON message to the remote server
+	// @param message A JSON formatted message
+	private void send(String message) {
+		try {
+			out.write(message);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Get the response from the remote server.
+	private InstantiatedSwitchRelation receive() {
+		try {
+			String decodedString;
+			System.out.println("Server response:");
+	
+			while ((decodedString = in.readLine()) != null) {
+				System.out.println(decodedString);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	private DFSStrategy dfsStrategy;
 	private String host;
+	private STS sts;
+	private HttpURLConnection conn;
+	private Writer out;
+	private BufferedReader in;
+	private boolean useDFS = true;
+	
+	/*
+	 * A Symbolic Transition System.
+	 * This contains the sts representing the GTS explored so far.
+	 */
+	private class STS {
+
+		private Location current;
+		private Map<HostGraph, Location> locationMapping;
+		private Map<Rule, SwitchRelation> switchRelationMapping;
+		
+		public STS() {
+			this.locationMapping = new HashMap<HostGraph, Location>();
+			this.switchRelationMapping = new HashMap<Rule, SwitchRelation>();
+		}
+		
+		public Location getCurrentLocation() {
+			return current;
+		}
+		
+		public void toLocation(Location l) {
+			current = l;
+		}
+		
+		public SwitchRelation getSwitchRelation(Rule rule) {
+			return switchRelationMapping.get(rule);
+		}
+		
+		public Location getLocation(HostGraph graph) {
+			return locationMapping.get(graph);
+		}
+		
+		public Location hostGraphToLocation(HostGraph graph) {
+			HostGraph locationGraph = generalize(graph);
+			Location location = locationMapping.get(locationGraph);
+			if (location == null) {
+				location = new RemoteStrategy.Location("s"+locationMapping.size());
+				locationMapping.put(locationGraph, location);
+			}
+			return location;
+		}
+		
+		public SwitchRelation ruleToSwitchRelation(Rule rule) {
+			SwitchRelation switchRelation = switchRelationMapping.get(rule);
+			if (switchRelation == null) {
+				switchRelation = new RemoteStrategy.SwitchRelation(rule);
+				switchRelationMapping.put(rule, switchRelation);
+			}
+			return switchRelation;
+		}
+		
+		public String toJSON() {
+			return "";
+		}
+		
+		private HostGraph generalize(HostGraph graph) {
+			HostGraph generalizedGraph = graph.clone();
+			System.out.println("nodes:");
+			for (HostNode node : generalizedGraph.nodeSet()) {
+				System.out.println(node.toString());
+			}
+			System.out.println("edges:");
+			for (HostEdge edge : generalizedGraph.edgeSet()) {
+				System.out.println(edge.toString());
+			}
+			return generalizedGraph;
+		}
+	}
+	
+	/*
+	 * A location in the STS.
+	 */
+	private class Location {
+		
+		private String identifier;
+		private Map<SwitchRelation, Location> relations;
+		
+		public Location(String identifier) {
+			this.identifier = identifier;
+			relations = new HashMap<SwitchRelation, Location>();
+		}
+		
+		public Set<SwitchRelation> getSwitchRelations() {
+			return relations.keySet();
+		}
+		
+		public Location getRelationTarget(SwitchRelation sr) {
+			return relations.get(sr);
+		}
+		
+		public void addSwitchRelation(SwitchRelation sr, Location l) {
+			relations.put(sr, l);
+		}
+		
+		public boolean explored() {
+			return !this.relations.isEmpty();
+		}
+		
+		public String toString() {
+			return identifier;
+		}
+	
+	}
+	
+	/*
+	 * A switch relation in the STS.
+	 */
+	private class SwitchRelation {
+		
+		private String gate;
+		private String guard;
+		private String update;
+		
+		public SwitchRelation(String gate, String guard, String update) {
+			this.gate = gate;
+			this.guard = guard;
+			this.update = update;
+		}
+		
+		public SwitchRelation(Rule rule) {
+			this.gate = rule.getFullName();
+			RuleGraph lhs = rule.lhs();
+			RuleGraph rhs = rule.rhs();
+			Condition nac = rule.getCondition();
+			System.out.println(lhs.toString());
+			System.out.println(rhs.toString());
+			System.out.println(nac.toString());
+		}
+		
+		public String toJSON() {
+			return "["+guard+","+update+"]";
+		}
+		
+	}
+	
+	private class InstantiatedSwitchRelation {
+		
+		private SwitchRelation relation;
+		private RuleTransition transition;
+		
+		public InstantiatedSwitchRelation(SwitchRelation relation, RuleTransition transition) {
+			this.relation = relation;
+			this.transition = transition;
+		}
+		
+		public SwitchRelation getSwitchRelation() {
+			return this.relation;
+		}
+		
+		public GraphTransition getTransition() {
+			return this.transition;
+		}
+	}
 }
