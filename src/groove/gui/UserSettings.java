03c6ce012ccb17a9d987392d072f9af0b12bfca1
Arend Rensink <arend.rensink@utwente.nl>
2012-07-03 05:29:07 +0000

    SF Feature Request #3527292

--- src/groove/gui/UserSettings.java
+++ src/groove/gui/UserSettings.java
@@ -14,22 +14,34 @@
  */
 package groove.gui;
 
+import groove.io.store.SystemStore;
+import groove.io.store.SystemStoreFactory;
+
+import java.io.IOException;
+import java.util.prefs.Preferences;
+
 import javax.swing.JFrame;
 import javax.swing.JSplitPane;
+import javax.swing.SwingUtilities;
 
 /**
  * Class that saves some basic information on the status of the Simulator.
  * @author Eduardo Zambon
  */
 public class UserSettings {
+    /** Reads and applies previously stored settings. */
+    public static void applyUserSettings(Simulator simulator) {
+        applyFrameSettings(simulator.getFrame());
+        applyLocationSettings(simulator);
+    }
 
     /** Reads and applies previously stored settings. */
-    public static void applyUserSettings(JFrame frame) {
-        String simMax = Options.userPrefs.get(SIM_MAX, "");
-        String simWidth = Options.userPrefs.get(SIM_WIDTH, "");
-        String simHeight = Options.userPrefs.get(SIM_HEIGHT, "");
-        String rulePos = Options.userPrefs.get(RULE_GRAPH_DIV_POS, "");
-        String mainPos = Options.userPrefs.get(MAIN_DIV_POS, "");
+    private static void applyFrameSettings(JFrame frame) {
+        String simMax = userPrefs.get(SIM_MAX_KEY, "");
+        String simWidth = userPrefs.get(SIM_WIDTH_KEY, "");
+        String simHeight = userPrefs.get(SIM_HEIGHT_KEY, "");
+        String rulePos = userPrefs.get(RULE_GRAPH_DIV_POS_KEY, "");
+        String mainPos = userPrefs.get(MAIN_DIV_POS_KEY, "");
 
         if (simMax.isEmpty() || simWidth.isEmpty() || simHeight.isEmpty()
             || rulePos.isEmpty() || mainPos.isEmpty()) {
@@ -74,26 +86,68 @@ public class UserSettings {
         return jsp.getDividerLocation();
     }
 
-    /** Synchronizes saved settings with the current ones. */
-    public static void synchSettings(JFrame frame) {
+    /** Restores the persisted user preferences into a given simulator. */
+    private static void applyLocationSettings(final Simulator simulator) {
+        String location = userPrefs.get(LOCATION_KEY, "");
+        if (!location.isEmpty()) {
+            // Set the value back so that an error in loading does not
+            // reoccur forever from now on
+            userPrefs.remove(LOCATION_KEY);
+            try {
+                final SystemStore store = SystemStoreFactory.newStore(location);
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            simulator.getActions().getLoadGrammarAction().load(
+                                store);
+                        } catch (IOException e) {
+                            // don't load if we're going to be difficult
+                        }
+                    }
+                });
+            } catch (IOException e) {
+                // don't load if we're going to be difficult
+            }
+        }
+    }
+
+    /** Synchronises saved settings with the current ones. */
+    public static void syncSettings(Simulator simulator) {
+        syncFrameSettings(simulator.getFrame());
+        syncLocationSettings(simulator);
+    }
+
+    /** Synchronises saved settings with the current ones. */
+    private static void syncFrameSettings(JFrame frame) {
         String simMax = Boolean.toString(isFrameMaximized(frame));
         String simWidth = Integer.toString(getFrameWidth(frame));
         String simHeight = Integer.toString(getFrameHeight(frame));
         String rulePos = Integer.toString(getRuleDivPos(frame));
         String mainPos = Integer.toString(getMainDivPos(frame));
 
-        Options.userPrefs.put(SIM_MAX, simMax);
-        Options.userPrefs.put(SIM_WIDTH, simWidth);
-        Options.userPrefs.put(SIM_HEIGHT, simHeight);
-        Options.userPrefs.put(RULE_GRAPH_DIV_POS, rulePos);
-        Options.userPrefs.put(MAIN_DIV_POS, mainPos);
+        userPrefs.put(SIM_MAX_KEY, simMax);
+        userPrefs.put(SIM_WIDTH_KEY, simWidth);
+        userPrefs.put(SIM_HEIGHT_KEY, simHeight);
+        userPrefs.put(RULE_GRAPH_DIV_POS_KEY, rulePos);
+        userPrefs.put(MAIN_DIV_POS_KEY, mainPos);
     }
 
-    static private final String SIM_MAX = "Simulator maximized";
-    static private final String SIM_WIDTH = "Simulator width";
-    static private final String SIM_HEIGHT = "Simulator height";
-    static private final String RULE_GRAPH_DIV_POS =
-        "Rule-Graph divider position";
-    static private final String MAIN_DIV_POS = "Main panel divider position";
+    /** Persists the state of the simulator into the user preferences. */
+    private static void syncLocationSettings(Simulator simulator) {
+        Object location = simulator.getModel().getStore().getLocation();
+        userPrefs.put(LOCATION_KEY, location.toString());
+    }
 
+    /** The persistently stored user preferences. */
+    private static final Preferences userPrefs = Options.userPrefs;
+    /** Key for the grammar location. */
+    private static final String LOCATION_KEY = "Grammar location";
+    static private final String SIM_MAX_KEY = "Simulator maximized";
+    static private final String SIM_WIDTH_KEY = "Simulator width";
+    static private final String SIM_HEIGHT_KEY = "Simulator height";
+    static private final String RULE_GRAPH_DIV_POS_KEY =
+        "Rule-Graph divider position";
+    static private final String MAIN_DIV_POS_KEY =
+        "Main panel divider position";
 }
