f5d09d68ebddb152e214dc334488b14c2d8f4e7f
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2012-11-08 17:58:01 +0000

    Adjusted classpath

--- groove/trunk/src/groove/gui/action/ImportAction.java
+++ groove/trunk/src/groove/gui/action/ImportAction.java
@@ -3,7 +3,6 @@ package groove.gui.action;
 import groove.gui.Icons;
 import groove.gui.Options;
 import groove.gui.Simulator;
-import groove.io.external.ConceptualPorter;
 import groove.io.external.Importer;
 
 import java.io.IOException;
@@ -17,7 +16,7 @@ public class ImportAction extends SimulatorAction {
         super(simulator, Options.IMPORT_ACTION_NAME, Icons.IMPORT_ICON);
 
         //TODO: Inits are pretty much a hack
-        ConceptualPorter.initSimulator(getSimulator());
+        //ConceptualPorter.initSimulator(getSimulator());
     }
 
     @Override
