413202d9dbac75bf58fa23f78fec23a0c21c859c
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2013-04-30 08:51:08 +0000

    Added utility for building Antlr 4 parse trees

--- groove/trunk/src/groove/util/Antlr4Grapher.java
+++ groove/trunk/src/groove/util/Antlr4Grapher.java
@@ -14,6 +14,8 @@ import groove.grammar.type.TypeLabel;
 import groove.grammar.type.TypeNode;
 import groove.graph.EdgeRole;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 import java.util.BitSet;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -26,34 +28,27 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 /**
  * Objects of this class can construct instance graphs and a type graph
- * for the ASTs of a particular Antlr 4 parser.
+ * for the parse trees of a particular Antlr 4 parser.
  * @author Arend Rensink
  * @version $Revision$
  */
 public class Antlr4Grapher {
     /** 
-     * Constructs an grapher for a particular Antlr parser.
+     * Constructs an grapher for a particular Antlr 4 parser.
      * @param parser the parser class that this object should construct graphs for
      * @param textTypes the token types for which the text should be stored 
      * in the graph (as string attributes)
-     * @throws IllegalArgumentException if the parser class doesn't define an accessible
-     * static array {@code String[] tokenNames}, or the value of one of
-     * the {@code textTypes} is not a valid index in this array
+     * @throws IllegalArgumentException if the parser class doesn't define 
+     * the expected static fields, or the value of one of
+     * the {@code textTypes} is not a valid token index
      */
     public Antlr4Grapher(Class<? extends Parser> parser, int... textTypes)
         throws IllegalArgumentException {
-        try {
-            this.tokens = (String[]) parser.getField(TOKEN_NAMES).get(null);
-        } catch (SecurityException e) {
-            throw new IllegalArgumentException(e);
-        } catch (IllegalAccessException e) {
-            throw new IllegalArgumentException(e);
-        } catch (NoSuchFieldException e) {
-            throw new IllegalArgumentException(e);
-        }
-        this.textTypes = new BitSet(this.tokens.length);
+        this.tokenNames = extractTokenNames(parser);
+        this.ruleNames = extractRuleNames(parser);
+        this.textTypes = new BitSet(this.tokenNames.size());
         for (int type : textTypes) {
-            if (type < 0 || type > this.tokens.length) {
+            if (!this.tokenNames.containsKey(type)) {
                 throw new IllegalArgumentException(String.format(
                     "Token type %d does not exist in parser class %s", type,
                     parser));
@@ -61,6 +56,71 @@ public class Antlr4Grapher {
                 this.textTypes.set(type);
             }
         }
+    }
+
+    /** 
+     * Extracts map from token numbers to symbolic token names 
+     * by reflection from the parser class file.
+     */
+    private Map<Integer,String> extractTokenNames(Class<? extends Parser> parser) {
+        Map<Integer,String> result = new HashMap<Integer,String>();
+        for (Field field : parser.getDeclaredFields()) {
+            String fieldName = field.getName();
+            if (fieldName.equals("tokenNames")) {
+                // after this field the generated parser contains no more tokens
+                break;
+            }
+            int modifiers = field.getModifiers();
+            if (!Modifier.isStatic(modifiers)) {
+                continue;
+            }
+            if (!Modifier.isPublic(modifiers)) {
+                continue;
+            }
+            if (!Modifier.isFinal(modifiers)) {
+                continue;
+            }
+            if (field.getType() != int.class) {
+                continue;
+            }
+            try {
+                Integer index = (Integer) field.get(null);
+                result.put(index, fieldName);
+            } catch (IllegalAccessException exc) {
+                throw new IllegalArgumentException(exc);
+            }
+        }
+        return result;
+    }
+
+    /** Extracts the set of symbolic rule names by reflection from the parser class file. */
+    private Set<String> extractRuleNames(Class<? extends Parser> parser) {
+        Set<String> result = new HashSet<String>();
+        for (Class<?> claz : parser.getDeclaredClasses()) {
+            int modifiers = claz.getModifiers();
+            if (!Modifier.isStatic(modifiers)) {
+                continue;
+            }
+            if (!Modifier.isPublic(modifiers)) {
+                continue;
+            }
+            String name = toRuleName(claz);
+            if (name != null) {
+                result.add(name);
+            }
+        }
+        return result;
+    }
+
+    /** Extracts a rule name from a class, if the class name ends on {@link #RULE_SUFFIX}. */
+    private String toRuleName(Class<?> claz) {
+        String result = null;
+        String name = claz.getSimpleName();
+        if (name.endsWith(RULE_SUFFIX)) {
+            result = name.substring(0, name.length() - RULE_SUFFIX.length());
+        }
+        return result;
+
     }
 
     /** Returns the type graph for this parser. */
@@ -73,21 +133,32 @@ public class Antlr4Grapher {
         result.addEdge(topNode, LAST_FLAG, topNode);
         result.addEdge(topNode, LEAF_FLAG, topNode);
         TypeNode stringNode = result.addNode(STRING_TYPE);
-        for (int i = 0; i < this.tokens.length; i++) {
-            String token = this.tokens[i];
+        for (Map.Entry<Integer,String> tokenEntry : this.tokenNames.entrySet()) {
+            String token = tokenEntry.getValue();
             if (ExprParser.isIdentifier(token)) {
-                TypeLabel typeLabel =
-                    TypeLabel.createLabel(EdgeRole.NODE_TYPE, token);
-                TypeNode tokenNode = result.addNode(typeLabel);
-                try {
-                    result.addInheritance(tokenNode, topNode);
-                } catch (FormatException e) {
-                    assert false;
-                }
-                if (this.textTypes.get(i)) {
+                TypeNode tokenNode = addType(result, topNode, token);
+                if (this.textTypes.get(tokenEntry.getKey())) {
                     result.addEdge(tokenNode, TEXT_LABEL, stringNode);
                 }
             }
+        }
+        for (String rule : this.ruleNames) {
+            addType(result, topNode, rule);
+        }
+        return result;
+    }
+
+    /**
+     * Adds a type node to a given type graph, and makes it a subtype of an
+     * already existing type node.
+     */
+    private TypeNode addType(TypeGraph typeGraph, TypeNode topNode, String name) {
+        TypeLabel typeLabel = TypeLabel.createLabel(EdgeRole.NODE_TYPE, name);
+        TypeNode result = typeGraph.addNode(typeLabel);
+        try {
+            typeGraph.addInheritance(result, topNode);
+        } catch (FormatException e) {
+            assert false;
         }
         return result;
     }
@@ -108,6 +179,9 @@ public class Antlr4Grapher {
             for (int i = 0; i < next.getChildCount(); i++) {
                 ParseTree child = next.getChild(i);
                 HostNode childNode = createNode(result, child);
+                if (childNode == null) {
+                    continue;
+                }
                 treeNodeMap.put(child, childNode);
                 result.addEdge(nextNode, CHILD_LABEL, childNode);
                 if (prevChild == null) {
@@ -128,27 +202,43 @@ public class Antlr4Grapher {
     }
 
     private HostNode createNode(DefaultHostGraph graph, ParseTree tree) {
-        HostNode result = graph.addNode();
-        int tokenType = ((Token) tree.getPayload()).getType();
-        graph.addEdge(result,
-            TypeLabel.createLabel(EdgeRole.NODE_TYPE, this.tokens[tokenType]),
-            result);
-        if (this.textTypes.get(tokenType) && tree.getText() != null) {
-            ValueNode nameNode =
-                graph.addNode(JavaStringAlgebra.instance, tree.getText());
-            graph.addEdge(result, TEXT_LABEL, nameNode);
+        HostNode result = null;
+        String type;
+        String text = null;
+        Object payload = tree.getPayload();
+        if (payload instanceof Token) {
+            // this is a leaf node; only include if it has text
+            int tokenType = ((Token) payload).getType();
+            type = this.tokenNames.get(tokenType);
+            if (this.textTypes.get(tokenType)) {
+                text = tree.getText();
+            }
+        } else {
+            type = toRuleName(payload.getClass());
+        }
+        if (type != null) {
+            // create the node and type it
+            result = graph.addNode();
+            graph.addEdge(result,
+                TypeLabel.createLabel(EdgeRole.NODE_TYPE, type), result);
+            // give it a text attribute if appropriate
+            if (text != null) {
+                ValueNode nameNode =
+                    graph.addNode(JavaStringAlgebra.instance, tree.getText());
+                graph.addEdge(result, TEXT_LABEL, nameNode);
+            }
         }
         return result;
     }
 
     /** List of token names. */
-    private final String[] tokens;
+    private final Map<Integer,String> tokenNames;
+    /** List of rule names. */
+    private final Set<String> ruleNames;
     /** Set of token types for which the text representation should be stored
      * in the graph as a string attribute.
      */
     private final BitSet textTypes;
-
-    private static final String TOKEN_NAMES = "tokenNames";
 
     /** Default label to be used for child edges. */
     public final static TypeLabel CHILD_LABEL =
@@ -174,5 +264,5 @@ public class Antlr4Grapher {
     /** String type label. */
     private final static TypeLabel STRING_TYPE = TypeLabel.createLabel(
         EdgeRole.NODE_TYPE, "string");
-    /** Subtype edge label. */
+    private final static String RULE_SUFFIX = "Context";
 }
