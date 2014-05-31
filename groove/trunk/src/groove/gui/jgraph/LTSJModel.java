76b799c483b41ea3226edae723e88e77606dc17d
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2014-05-31 15:53:16 +0000

    Fixed SF Bug #427

--- groove/trunk/src/groove/gui/jgraph/LTSJModel.java
+++ groove/trunk/src/groove/gui/jgraph/LTSJModel.java
@@ -1,17 +1,17 @@
 /*
  * GROOVE: GRaphs for Object Oriented VErification Copyright 2003--2007
  * University of Twente
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
  * License for the specific language governing permissions and limitations under
  * the License.
- * 
+ *
  * $Id: LTSJModel.java,v 1.28 2008-03-05 16:49:24 rensink Exp $
  */
 package groove.gui.jgraph;
@@ -96,30 +96,32 @@ final public class LTSJModel extends JModel<GTS> implements GTSListener {
 
     @Override
     public void statusUpdate(GTS lts, GraphState explored, Flag flag) {
-        JVertex<GTS> jCell = getJCellForNode(explored);
-        switch (flag) {
-        case CLOSED:
-            jCell.setLook(Look.OPEN, false);
-            break;
-        case ERROR:
-            jCell.setStale(VisualKey.ERROR);
-            break;
-        case DONE:
-            if (explored.isAbsent()) {
-                for (JEdge<GTS> jEdge : jCell.getContext()) {
-                    jEdge.setLook(Look.ABSENT, true);
-                }
-                jCell.setLook(Look.ABSENT, true);
-            }
-            if (explored.isRecipeState()) {
-                for (JEdge<GTS> jEdge : jCell.getContext()) {
-                    jEdge.setLook(Look.RECIPE, true);
-                }
-                jCell.setLook(Look.RECIPE, true);
-            }
+        if (this.listening) {
+            JVertex<GTS> jCell = getJCellForNode(explored);
             if (jCell != null) {
-                jCell.setLook(Look.FINAL, lts.isFinal(explored));
-                jCell.setLook(Look.RESULT, lts.isResult(explored));
+                switch (flag) {
+                case CLOSED:
+                    jCell.setLook(Look.OPEN, false);
+                    break;
+                case ERROR:
+                    jCell.setStale(VisualKey.ERROR);
+                    break;
+                case DONE:
+                    if (explored.isAbsent()) {
+                        for (JEdge<GTS> jEdge : jCell.getContext()) {
+                            jEdge.setLook(Look.ABSENT, true);
+                        }
+                        jCell.setLook(Look.ABSENT, true);
+                    }
+                    if (explored.isRecipeState()) {
+                        for (JEdge<GTS> jEdge : jCell.getContext()) {
+                            jEdge.setLook(Look.RECIPE, true);
+                        }
+                        jCell.setLook(Look.RECIPE, true);
+                    }
+                    jCell.setLook(Look.FINAL, lts.isFinal(explored));
+                    jCell.setLook(Look.RESULT, lts.isResult(explored));
+                }
             }
         }
     }
