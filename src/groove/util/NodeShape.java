2b85e7ab05fe61fb3192d12532fa792c21b872e2
Arend Rensink <arend.rensink@utwente.nl>
2014-06-10 20:41:28 +0000

    Hexagonal shapes for transient nodes

--- src/groove/util/NodeShape.java
+++ src/groove/util/NodeShape.java
@@ -27,6 +27,201 @@ import java.awt.geom.Rectangle2D;
 public enum NodeShape {
     /** Rounded rectangle shape. */
     ROUNDED,
+    /** Hexagonal shape. */
+    HEXAGON {
+        @Override
+        Point2D getPerimeterPoint(Rectangle2D bounds, double px, double py, Point2D q) {
+            Point2D result = null;
+            this.bounds = bounds;
+            this.x2 = px;
+            this.dx2 = q.getX() - px;
+            this.y2 = py;
+            this.dy2 = q.getY() - py;
+            this.nonnull = false;
+            if (q.getY() < bounds.getY()) {
+                // q lies in the top half
+                result = getTopLeft();
+                if (result == null) {
+                    result = getTop();
+                }
+                if (result == null) {
+                    result = getTopRight();
+                }
+            } else if (q.getY() > bounds.getMaxY()) {
+                // q lies in the bottom half
+                result = getBottomLeft();
+                if (result == null) {
+                    result = getBottom();
+                }
+                if (result == null) {
+                    result = getBottomRight();
+                }
+            } else if (px > q.getX()) {
+                // q lies to the left
+                result = getTop();
+                if (result == null) {
+                    result = getTopLeft();
+                }
+                if (result == null) {
+                    result = getBottomLeft();
+                }
+                if (result == null) {
+                    result = getBottom();
+                }
+            } else {
+                // q lies to the right
+                result = getTop();
+                if (result == null) {
+                    result = getTopRight();
+                }
+                if (result == null) {
+                    result = getBottomRight();
+                }
+                if (result == null) {
+                    result = getBottom();
+                }
+            }
+            return result;
+        }
+
+        @Override
+        Point2D getPerimeterPoint(double w, double h, double dx, double dy) {
+            Point2D result = null;
+            this.bounds = new Rectangle2D.Double(-w, -h, 2 * w, 2 * h);
+            this.x2 = 0;
+            this.dx2 = dx;
+            this.y2 = 0;
+            this.dy2 = dy;
+            this.nonnull = true;
+            double ratio = w / h - 2 * HEX_EXTEND_RATIO;
+            if (dy < 0) {
+                // q lies in the top half
+                double angle = dx / dy;
+                if (angle > ratio) {
+                    result = getTopLeft();
+                } else if (angle < -ratio) {
+                    result = getTopRight();
+                } else {
+                    result = getTop();
+                }
+                assert result != null;
+            } else if (dy > 0) {
+                // point lies in bottom half
+                double angle = dx / dy;
+                if (angle < -ratio) {
+                    result = getBottomLeft();
+                } else if (angle > ratio) {
+                    result = getBottomRight();
+                } else {
+                    result = getBottom();
+                }
+                assert result != null;
+            } else if (dx < 0) {
+                result = new Point2D.Double(-w, 0);
+            } else {
+                result = new Point2D.Double(w, 0);
+            }
+            return result;
+        }
+
+        /** Returns the intersection of the top left border. */
+        private Point2D getTopLeft() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getX();
+            double y1 = this.bounds.getY() + height / 2;
+            double dx1 = extend;
+            double dy1 = -height / 2;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of the top border. */
+        private Point2D getTop() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getX() + extend;
+            double y1 = this.bounds.getY();
+            double dx1 = this.bounds.getWidth() - 2 * extend;
+            double dy1 = 0;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of the top right border. */
+        private Point2D getTopRight() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getMaxX() - extend;
+            double y1 = this.bounds.getY();
+            double dx1 = extend;
+            double dy1 = height / 2;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of the bottom right border. */
+        private Point2D getBottomRight() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getMaxX();
+            double y1 = this.bounds.getY() + height / 2;
+            double dx1 = -extend;
+            double dy1 = height / 2;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of the bottom border. */
+        private Point2D getBottom() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getMaxX() - extend;
+            double y1 = this.bounds.getMaxY();
+            double dx1 = -this.bounds.getWidth() - 2 * extend;
+            double dy1 = 0;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of the bottom left border. */
+        private Point2D getBottomLeft() {
+            double height = this.bounds.getHeight();
+            double extend = height * HEX_EXTEND_RATIO;
+            double x1 = this.bounds.getX();
+            double y1 = this.bounds.getY() + height / 2;
+            double dx1 = extend;
+            double dy1 = height / 2;
+            return intersect(x1, y1, dx1, dy1);
+        }
+
+        /** Returns the intersection of a given vector {@code v1 = (x1,y1) + (dx1,dy1)}
+         * with the stored vector {@code (x2,y2) + (dx2,dy2)}, provided the point 
+         * actually lies on the {@code v1} segment; or {@code null} otherwise.
+         */
+        private Point2D intersect(double x1, double y1, double dx1, double dy1) {
+            Point2D result =
+                lineIntersection(x1, y1, dx1, dy1, this.x2, this.y2, this.dx2, this.dy2);
+            if (result != null) {
+                double x = result.getX();
+                double y = result.getY();
+                if ((x1 - EPS < x) && (x > x1 + dx1 + EPS)) {
+                    assert !this.nonnull;
+                    result = null;
+                } else if (x1 + dx1 - EPS < x && x > x1 + EPS) {
+                    assert !this.nonnull;
+                    result = null;
+                } else if (y1 - EPS < y && y > y1 + dy1 + EPS) {
+                    assert !this.nonnull;
+                    result = null;
+                } else if (y1 + dy1 - EPS < y && y > y1 + EPS) {
+                    assert !this.nonnull;
+                    result = null;
+                }
+            }
+            return result;
+        }
+
+        private Rectangle2D bounds;
+        private double x2, y2, dx2, dy2;
+        private boolean nonnull;
+        private static final double EPS = 0.001;
+    },
     /** Ellipse (or circle) shape. */
     ELLIPSE {
         /* Overridden for ellipses. */
@@ -252,7 +447,7 @@ public enum NodeShape {
         double dxLeft = px - bounds.getMinX();
         double dyBottom = bounds.getMaxY() - py;
         double dyTop = py - bounds.getMinY();
-        // angles to upper left, upper right, bottom left, bottom right corner
+        // angles from source point to upper left, upper right, bottom left, bottom right corner
         double urPhi = Math.atan2(-dyTop, dxRight);
         double ulPhi = Math.atan2(-dyTop, -dxLeft);
         double brPhi = Math.atan2(dyBottom, dxRight);
@@ -380,21 +575,23 @@ public enum NodeShape {
      * @param y2 Start point of the second line (y-coordinate)
      * @param dx2 vector of the second line (x-direction)
      * @param dy2 vector of the second line (y-direction)
-     * @return Intersection point of the two lines, of <code>null</code> if they
-     *         are parallel
+     * @return Intersection point of the two lines, of <code>null</code> if 
+     * the lines do not intersect in the given interval.
      */
     public static Point2D lineIntersection(double x1, double y1, double dx1, double dy1, double x2,
             double y2, double dx2, double dy2) {
-        double above = dx1 * (y2 - y1) - dy1 * (x2 - x1);
+        Point2D result = null;
         double below = dx2 * dy1 - dx1 * dy2;
-        if (below == 0) {
-            // the lines are parallel
-            return null;
-        } else {
+        if (below != 0) {
+            double above = dx1 * (y2 - y1) - dy1 * (x2 - x1);
             double c2 = above / below;
             double x = x2 + dx2 * c2;
             double y = y2 + dy2 * c2;
-            return new Point2D.Double(x, y);
+            result = new Point2D.Double(x, y);
         }
+        return result;
     }
+
+    /** Ratio of the left and right extensions of a hexagon to the height. */
+    public static final double HEX_EXTEND_RATIO = .2;
 }
