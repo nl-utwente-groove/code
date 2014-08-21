f9f18cb94bb846dd37a49d9eed43ec1fc4e8ae98
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2014-08-21 19:19:42 +0000

    Refactoring to accomodate more flexible editors

--- groove/trunk/src/groove/gui/dialog/config/NullEditor.java
+++ groove/trunk/src/groove/gui/dialog/config/NullEditor.java
@@ -23,8 +23,6 @@ import groove.explore.config.SettingKey;
 import groove.gui.dialog.ConfigDialog;
 import groove.util.parse.FormatException;
 
-import java.awt.CardLayout;
-
 import javax.swing.JPanel;
 
 /**
@@ -32,70 +30,19 @@ import javax.swing.JPanel;
  * @author Arend Rensink
  * @version $Revision $
  */
-public class NullEditor extends SettingEditor {
+public class NullEditor extends ContentEditor {
     /**
      * Constructs a null editor for a given exploration key and setting kind.
      * @param dialog the configuration dialog for which this is an editor
      */
     public NullEditor(ConfigDialog<?> dialog, JPanel holder, ExploreKey key, SettingKey kind) {
+        super(dialog, holder, key, kind);
         assert kind.getContentType() == Null.class;
-        this.dialog = dialog;
-        this.holder = holder;
-        this.key = key;
-        this.kind = kind;
-        if (holder != null) {
-            holder.add(this, kind.getName());
-        }
     }
-
-    private ConfigDialog<?> getDialog() {
-        return this.dialog;
-    }
-
-    private final ConfigDialog<?> dialog;
-
-    /** Tests if this editor is placed on a parent container. */
-    private boolean hasHolder() {
-        return getHolder() != null;
-    }
-
-    /** Returns the parent container on which this editor is placed, if any. */
-    private JPanel getHolder() {
-        return this.holder;
-    }
-
-    /**
-     * The parent panel, on which this is placed.
-     * May be {@code null} if the explore key has no content at all.
-     */
-    private final JPanel holder;
-
-    @Override
-    public ExploreKey getKey() {
-        return this.key;
-    }
-
-    private final ExploreKey key;
-
-    @Override
-    public SettingKey getKind() {
-        return this.kind;
-    }
-
-    private final SettingKey kind;
 
     @Override
     public void refresh() {
         // does nothing
-    }
-
-    @Override
-    public void activate() {
-        if (hasHolder()) {
-            CardLayout layout = (CardLayout) getHolder().getLayout();
-            layout.show(getHolder(), getKind().getName());
-            getDialog().setError(getKey(), getError());
-        }
     }
 
     @Override
@@ -106,11 +53,6 @@ public class NullEditor extends SettingEditor {
     @Override
     public void setSetting(Setting<?,?> content) {
         assert content.getKind() == getKind();
-    }
-
-    @Override
-    public boolean hasError() {
-        return getError() != null;
     }
 
     @Override
