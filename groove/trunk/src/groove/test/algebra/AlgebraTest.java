bf249a10421d02dd44c6b4c9dea3ed772a0ab499
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2015-01-28 19:05:14 +0000

    Fixed bug due to previous commit

--- groove/trunk/src/groove/test/algebra/AlgebraTest.java
+++ groove/trunk/src/groove/test/algebra/AlgebraTest.java
@@ -1,15 +1,15 @@
 /* GROOVE: GRaphs for Object Oriented VErification
  * Copyright 2003--2011 University of Twente
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * $Id$
@@ -32,14 +32,10 @@ public abstract class AlgebraTest<B,I,R,S> {
     @SuppressWarnings("unchecked")
     AlgebraTest(AlgebraFamily family) {
         this.family = family;
-        this.intAlgebra =
-            (IntAlgebra<I,B,S>) family.getAlgebra(Sort.INT);
-        this.boolAlgebra =
-            (BoolAlgebra<B>) family.getAlgebra(Sort.BOOL);
-        this.realAlgebra =
-            (RealAlgebra<R,B,S>) family.getAlgebra(Sort.REAL);
-        this.stringAlgebra =
-            (StringAlgebra<S,B,I>) family.getAlgebra(Sort.STRING);
+        this.intAlgebra = (IntAlgebra<I,R,B,S>) family.getAlgebra(Sort.INT);
+        this.boolAlgebra = (BoolAlgebra<B>) family.getAlgebra(Sort.BOOL);
+        this.realAlgebra = (RealAlgebra<R,B,S>) family.getAlgebra(Sort.REAL);
+        this.stringAlgebra = (StringAlgebra<S,B,I>) family.getAlgebra(Sort.STRING);
         this.TRUE = this.boolAlgebra.toValueFromJava(true);
         this.FALSE = this.boolAlgebra.toValueFromJava(false);
     }
@@ -333,7 +329,7 @@ public abstract class AlgebraTest<B,I,R,S> {
     }
 
     private final AlgebraFamily family;
-    final IntAlgebra<I,B,S> intAlgebra;
+    final IntAlgebra<I,R,B,S> intAlgebra;
     final BoolAlgebra<B> boolAlgebra;
     final RealAlgebra<R,B,S> realAlgebra;
     final StringAlgebra<S,B,I> stringAlgebra;
