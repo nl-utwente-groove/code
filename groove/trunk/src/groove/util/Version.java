ba000954d4ede293e44821e117d7712b7eb81871
zambon <zambon@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2015-08-25 19:20:37 +0000

    Updated version information.

--- groove/trunk/src/groove/util/Version.java
+++ groove/trunk/src/groove/util/Version.java
@@ -63,10 +63,10 @@ public class Version {
     }
 
     /** Build number (timestamp with format yyyyMMddHHmmssSSS). */
-    public static final String BUILD = "20150624";
+    public static final String BUILD = "20150825";
 
     /** Release date of this version (date format dd.MM.yyyy). */
-    public static final String DATE = "24.06.2015";
+    public static final String DATE = "25.08.2015";
 
     /**
      * Groove Version number of format x.y.z, with
@@ -77,7 +77,7 @@ public class Version {
      * </ul>
      * A '+' sign at the end of the number indicates a development version.
      */
-    public static final String NUMBER = "5.5.3+";
+    public static final String NUMBER = "5.5.4+";
 
     /** Minimum Java JRE version required. */
     static public final String NUMBER_JAVAMIN = "1.6";
