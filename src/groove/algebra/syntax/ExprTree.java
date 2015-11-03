16d1235b07ef389abd0b44a7d137cec39eeb826b
Arend Rensink <arend.rensink@utwente.nl>
2015-11-03 21:15:22 +0000

    Repaired error in type inference of typed expressions

--- src/groove/algebra/syntax/ExprTree.java
+++ src/groove/algebra/syntax/ExprTree.java
@@ -1,15 +1,15 @@
 /* GROOVE: GRaphs for Object Oriented VErification
  * Copyright 2003--2011 University of Twente
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * $Id$
@@ -73,7 +73,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
 
     private Sort sort;
 
-    /** 
+    /**
      * Converts this parse tree into an {@link Assignment}.
      */
     public Assignment toAssignment() throws FormatException {
@@ -134,7 +134,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         return result;
     }
 
-    /** 
+    /**
      * Returns the constant expression this tree represents, if any.
      * @return the constant expression this tree represents
      * @throws FormatException if this tree does not represent a constant
@@ -149,7 +149,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         return result;
     }
 
-    /** 
+    /**
      * Converts this tree to a multi-sorted {@link Variable} or a {@link FieldExpr}.
      * Chained field expressions are currently unsupported.
      * @param varMap variable typing
@@ -168,7 +168,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         return result;
     }
 
-    /** 
+    /**
      * Converts this tree to a {@link Variable} or a {@link FieldExpr}.
      * Chained field expressions are currently unsupported.
      * @param varMap variable typing
@@ -198,7 +198,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         return result;
     }
 
-    /** 
+    /**
      * Converts this tree to a {@link Variable} or a {@link Parameter}.
      * @param name variable name
      * @param sort expected type of the expression
@@ -233,6 +233,10 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
             resultArgs.add(arg.toExpressions(varMap));
         }
         for (Operator op : getOp().getOperators()) {
+            if (hasSort() && getSort() != op.getSort()) {
+                // the type of op does not correspond to the known operator type
+                continue;
+            }
             boolean duplicate = false;
             try {
                 duplicate = (result.put(op.getResultType(), newCallExpr(op, resultArgs)) != null);
@@ -254,7 +258,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
     /**
      * Factory method for a new operator expression.
      * @param op the operator of the new expression
-     * @param args operator arguments. Each argument is a map from 
+     * @param args operator arguments. Each argument is a map from
      * possible types to corresponding expressions
      * @throws FormatException if {@code args} does not have values
      * for the required operator types
@@ -279,8 +283,8 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         OpValue opValue = op.getOpValue();
         if ((opValue == IntSignature.Op.NEG || opValue == RealSignature.Op.NEG)
             && selectedArgs.get(0) instanceof Constant) {
-            return op.getResultType().createConstant(
-                op.getSymbol() + selectedArgs.get(0).toDisplayString());
+            return op.getResultType().createConstant(op.getSymbol()
+                + selectedArgs.get(0).toDisplayString());
         } else {
             return new CallExpr(hasSort(), op, selectedArgs);
         }
@@ -379,7 +383,7 @@ public class ExprTree extends TermTree<ExprTree.ExprOp,ExprTree> {
         }
 
         /** Returns the algebra operator of a given sort wrapped into this object,
-         * if any. 
+         * if any.
          * @param sort the non-{@code null} sort for which the operator is requested
          */
         public Operator getOperator(Sort sort) {
