2b9e89c4a1262dd12225bd1f38ceb52901aa6324
zambon <zambon@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2017-06-26 19:56:33 +0000

    Fixed bug #479.

--- groove/trunk/src/groove/io/external/util/TikzStylesExtractor.java
+++ groove/trunk/src/groove/io/external/util/TikzStylesExtractor.java
@@ -30,7 +30,6 @@ import static groove.gui.look.Look.CTRL_TRANSIENT_STATE;
 import static groove.gui.look.Look.CTRL_VERDICT;
 import static groove.gui.look.Look.DATA;
 import static groove.gui.look.Look.EMBARGO;
-import static groove.gui.look.Look.EQUIV_CLASS;
 import static groove.gui.look.Look.ERASER;
 import static groove.gui.look.Look.FINAL;
 import static groove.gui.look.Look.GRAYED_OUT;
@@ -38,7 +37,6 @@ import static groove.gui.look.Look.NESTING;
 import static groove.gui.look.Look.NODIFIED;
 import static groove.gui.look.Look.NO_ARROW;
 import static groove.gui.look.Look.OPEN;
-import static groove.gui.look.Look.PATTERN;
 import static groove.gui.look.Look.PRODUCT;
 import static groove.gui.look.Look.RECIPE;
 import static groove.gui.look.Look.REGULAR;
@@ -77,6 +75,13 @@ import static groove.gui.look.VisualKey.OPAQUE;
 import static groove.gui.look.VisualKey.POINTS;
 import static groove.gui.look.VisualKey.TEXT_SIZE;
 import static groove.gui.look.VisualKey.VISIBLE;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+
 import groove.gui.jgraph.JAttr;
 import groove.gui.look.EdgeEnd;
 import groove.gui.look.Look;
@@ -87,12 +92,6 @@ import groove.util.Duo;
 import groove.util.NodeShape;
 import groove.util.line.Line.ColorType;
 
-import java.awt.Color;
-import java.util.ArrayList;
-import java.util.EnumSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * Class to automatically create a groove2tikz.sty file from the existing
  * {@link Look} enumeration.
@@ -102,24 +101,71 @@ import java.util.Set;
 public final class TikzStylesExtractor {
 
     /** Enumeration of main looks that are defined. */
-    public static final Set<Look> mainLooks = EnumSet.of(BASIC, CREATOR, CONNECT, DATA, EMBARGO,
-        ERASER, NESTING, PRODUCT, REMARK, TYPE, ABSTRACT, SUBTYPE, STATE, TRANS, START, TRANSIENT,
-        CTRL_TRANSIENT_STATE);
+    public static final Set<Look> mainLooks = EnumSet.of(BASIC,
+        CREATOR,
+        CONNECT,
+        DATA,
+        EMBARGO,
+        ERASER,
+        NESTING,
+        PRODUCT,
+        REMARK,
+        TYPE,
+        ABSTRACT,
+        SUBTYPE,
+        STATE,
+        TRANS,
+        START,
+        TRANSIENT,
+        CTRL_TRANSIENT_STATE,
+        ADDER);
 
     /** Subset of the main looks that are suitable for nodes. */
-    private static final Set<Look> mainNodeLooks = EnumSet.of(BASIC, CREATOR, DATA, EMBARGO,
-        ERASER, NESTING, PRODUCT, REMARK, TYPE, ABSTRACT, STATE, START, Look.TRANSIENT,
-        CTRL_TRANSIENT_STATE);
+    private static final Set<Look> mainNodeLooks = EnumSet.of(BASIC,
+        CREATOR,
+        DATA,
+        EMBARGO,
+        ERASER,
+        NESTING,
+        PRODUCT,
+        REMARK,
+        TYPE,
+        ABSTRACT,
+        STATE,
+        START,
+        TRANSIENT,
+        CTRL_TRANSIENT_STATE,
+        ADDER);
 
     /** Subset of the main looks that are suitable for edges. */
-    private static final Set<Look> mainEdgeLooks = EnumSet.of(BASIC, CREATOR, CONNECT, EMBARGO,
-        ERASER, NESTING, REMARK, TYPE, ABSTRACT, SUBTYPE, TRANS);
+    private static final Set<Look> mainEdgeLooks = EnumSet.of(BASIC,
+        CREATOR,
+        CONNECT,
+        EMBARGO,
+        ERASER,
+        NESTING,
+        REMARK,
+        TYPE,
+        ABSTRACT,
+        SUBTYPE,
+        TRANS,
+        ADDER);
 
     /**
      * Extra enumeration for the additional looks that can modify a main look.
      */
-    private static final Set<Look> modifyingLooks = EnumSet.of(NODIFIED, BIDIRECTIONAL, NO_ARROW,
-        COMPOSITE, OPEN, FINAL, Look.ERROR, RESULT, RECIPE, ABSENT, ACTIVE, GRAYED_OUT,
+    private static final Set<Look> modifyingLooks = EnumSet.of(NODIFIED,
+        BIDIRECTIONAL,
+        NO_ARROW,
+        COMPOSITE,
+        OPEN,
+        FINAL,
+        Look.ERROR,
+        RESULT,
+        RECIPE,
+        ABSENT,
+        ACTIVE,
+        GRAYED_OUT,
         CTRL_VERDICT);
 
     /**
@@ -127,22 +173,43 @@ public final class TikzStylesExtractor {
      * unusedLooks to be equal to the entire Look enum, otherwise an error
      * is raised. This is needed to ensure the consistency of the extractor.
      */
-    public static final Set<Look> unusedLooks = EnumSet.of(REGULAR, ADDER, PATTERN, EQUIV_CLASS);
+    public static final Set<Look> unusedLooks = EnumSet.of(REGULAR);
 
     /**
      * Set of visual keys that are used in the extractor.
      */
-    private static final Set<VisualKey> usedKeys = EnumSet.of(BACKGROUND, DASH, EDGE_SOURCE_SHAPE,
-        EDGE_TARGET_SHAPE, FOREGROUND, LINE_WIDTH, NODE_SHAPE);
+    private static final Set<VisualKey> usedKeys = EnumSet.of(BACKGROUND,
+        DASH,
+        EDGE_SOURCE_SHAPE,
+        EDGE_TARGET_SHAPE,
+        FOREGROUND,
+        LINE_WIDTH,
+        NODE_SHAPE);
 
     /**
      * Set of visual keys that are not used in the extractor because they are
      * element dependent and hence cannot form a style.
      */
-    private static final Set<VisualKey> unusedKeys = EnumSet.of(ADORNMENT, COLOR,
-        EDGE_SOURCE_LABEL, EDGE_SOURCE_POS, EDGE_TARGET_LABEL, EDGE_TARGET_POS, EMPHASIS, ERROR,
-        FONT, INNER_LINE, INSET, LABEL, LABEL_POS, LINE_STYLE, NODE_POS, NODE_SIZE, TEXT_SIZE,
-        OPAQUE, POINTS, VISIBLE);
+    private static final Set<VisualKey> unusedKeys = EnumSet.of(ADORNMENT,
+        COLOR,
+        EDGE_SOURCE_LABEL,
+        EDGE_SOURCE_POS,
+        EDGE_TARGET_LABEL,
+        EDGE_TARGET_POS,
+        EMPHASIS,
+        ERROR,
+        FONT,
+        INNER_LINE,
+        INSET,
+        LABEL,
+        LABEL_POS,
+        LINE_STYLE,
+        NODE_POS,
+        NODE_SIZE,
+        TEXT_SIZE,
+        OPAQUE,
+        POINTS,
+        VISIBLE);
 
     /** Main method. */
     public static final void main(String[] args) {
@@ -172,7 +239,8 @@ public final class TikzStylesExtractor {
                 System.err.println("Did you add new entries in the Look enumeration?");
             }
             if (!allKeys.isEmpty()) {
-                System.err.println("Don't know how to handle the following visual keys: " + allKeys);
+                System.err
+                    .println("Don't know how to handle the following visual keys: " + allKeys);
                 System.err.println("Did you add new entries in the VisualKey enumeration?");
             }
             System.exit(1);
@@ -201,8 +269,8 @@ public final class TikzStylesExtractor {
         for (ColorType cType : ColorType.values()) {
             Color color = cType.getColor();
             if (color != null) {
-                append("\\definecolor{" + cType.name().toLowerCase() + "_c}{RGB}"
-                    + Style.getColorStringDefinition(color) + NEW_LINE);
+                append("\\definecolor{" + cType.name()
+                    .toLowerCase() + "_c}{RGB}" + Style.getColorStringDefinition(color) + NEW_LINE);
             }
         }
     }
@@ -229,7 +297,8 @@ public final class TikzStylesExtractor {
         for (Look modifyingLook : modifyingLooks) {
             styles.clear();
             append(BEGIN_TIKZ_STYLE);
-            append(modifyingLook.name().toLowerCase());
+            append(modifyingLook.name()
+                .toLowerCase());
             append(MID_TIKZ_STYLE);
             computeModifyingStyle(modifyingLook, styles);
             append(styles.toString());
@@ -305,56 +374,32 @@ public final class TikzStylesExtractor {
     private static final String ROUNDED_CORNERS_KEY = "rounded corners";
 
     private static final String HEADER =
-        "% Package that defines the styles used in Tikz figures exported in GROOVE."
-            + NEW_LINE
+        "% Package that defines the styles used in Tikz figures exported in GROOVE." + NEW_LINE
             + "% This file was automatically generated by the TikzStylesExtraction utility."
-            + NEW_LINE
-            + NEW_LINE
-            + "\\ProvidesPackage{groove2tikz}"
-            + NEW_LINE
-            + "\\RequirePackage{tikz}"
-            + NEW_LINE
-            + "\\usepackage[T1]{fontenc}"
-            + NEW_LINE
-            + "\\usepackage{amssymb}"
-            + NEW_LINE
-            + NEW_LINE
-            + "% Includes for Tikz."
-            + NEW_LINE
-            + "\\usetikzlibrary{arrows,automata,positioning,er}"
-            + NEW_LINE
-            + NEW_LINE
-            + "% Dimension styles."
-            + NEW_LINE
-            + "\\newcommand{\\tikzfontsize}{\\footnotesize}"
-            + NEW_LINE
-            + "\\newcommand{\\tikzscale}{2}"
-            + NEW_LINE
-            + NEW_LINE
+            + NEW_LINE + NEW_LINE + "\\ProvidesPackage{groove2tikz}" + NEW_LINE
+            + "\\RequirePackage{tikz}" + NEW_LINE + "\\usepackage[T1]{fontenc}" + NEW_LINE
+            + "\\usepackage{amssymb}" + NEW_LINE + NEW_LINE + "% Includes for Tikz." + NEW_LINE
+            + "\\usetikzlibrary{arrows,automata,positioning,er}" + NEW_LINE + NEW_LINE
+            + "% Dimension styles." + NEW_LINE + "\\newcommand{\\tikzfontsize}{\\footnotesize}"
+            + NEW_LINE + "\\newcommand{\\tikzscale}{2}" + NEW_LINE + NEW_LINE
             + "\\tikzstyle every node=[font=\\tikzfontsize\\sffamily, inner sep=2.5pt, minimum size=9pt]"
-            + NEW_LINE
-            + NEW_LINE
-            + "% Extra style for edge labels."
-            + NEW_LINE
-            + "\\tikzstyle{lab}=[fill=white, inner sep=1pt]"
-            + NEW_LINE
-            + NEW_LINE
-            + "% Extra style for parameter adornments."
-            + NEW_LINE
+            + NEW_LINE + NEW_LINE + "% Extra style for edge labels." + NEW_LINE
+            + "\\tikzstyle{lab}=[fill=white, inner sep=1pt]" + NEW_LINE + NEW_LINE
+            + "% Extra style for parameter adornments." + NEW_LINE
             + "\\tikzstyle{par_node}=[draw=black, fill=black, text=white, shape=rectangle, font=\\scriptsize\\sffamily, inner sep=1pt, minimum size=4pt, anchor=east]"
             + NEW_LINE + NEW_LINE + "% Default colors for TeX strings." + NEW_LINE;
 
     private static final String MAIN_STYLE_COMMENT = NEW_LINE
         + "% Main styles. (Should be used first in a node and edge definition.)" + NEW_LINE;
 
-    private static final String MOD_STYLE_COMMENT = NEW_LINE
-        + "% Modifying styles. (To be used in conjunction - AFTER - a main style.)" + NEW_LINE
-        + NEW_LINE;
+    private static final String MOD_STYLE_COMMENT =
+        NEW_LINE + "% Modifying styles. (To be used in conjunction - AFTER - a main style.)"
+            + NEW_LINE + NEW_LINE;
 
-    private static final String FOOTER = NEW_LINE
-        + "% Ugly hack to allow nodes with multiple lines." + NEW_LINE + "\\newcommand{\\ml}[1]{"
-        + NEW_LINE + "\\begin{tabular}{@{}c@{}}#1\\vspace{-2pt}\\end{tabular}" + NEW_LINE + "}"
-        + NEW_LINE;
+    private static final String FOOTER =
+        NEW_LINE + "% Ugly hack to allow nodes with multiple lines." + NEW_LINE
+            + "\\newcommand{\\ml}[1]{" + NEW_LINE
+            + "\\begin{tabular}{@{}c@{}}#1\\vspace{-2pt}\\end{tabular}" + NEW_LINE + "}" + NEW_LINE;
 
     /**
      * Auxiliary class for storing the useful Look information and outputing
@@ -387,14 +432,16 @@ public final class TikzStylesExtractor {
             sb.append(NEW_LINE);
             if (!this.nodes.isEmpty()) {
                 sb.append(BEGIN_TIKZ_STYLE);
-                sb.append(this.look.name().toLowerCase() + NODE_SUFFIX);
+                sb.append(this.look.name()
+                    .toLowerCase() + NODE_SUFFIX);
                 sb.append(MID_TIKZ_STYLE);
                 sb.append(this.nodes.toString());
                 sb.append(END_TIKZ_STYLE);
             }
             if (!this.edges.isEmpty()) {
                 sb.append(BEGIN_TIKZ_STYLE);
-                sb.append(this.look.name().toLowerCase() + EDGE_SUFFIX);
+                sb.append(this.look.name()
+                    .toLowerCase() + EDGE_SUFFIX);
                 sb.append(MID_TIKZ_STYLE);
                 sb.append(this.edges.toString());
                 sb.append(END_TIKZ_STYLE);
@@ -442,6 +489,7 @@ public final class TikzStylesExtractor {
                 writeLineWidth(this.lineWidth, this.nodes);
                 writeForegroundColor(this.foreground, this.nodes);
                 writeBackgroundColor(this.background, this.nodes);
+                writeNodePostaction(this.look, this.nodes);
             }
             // Edge styles.
             if (mainEdgeLooks.contains(this.look)) {
@@ -450,6 +498,7 @@ public final class TikzStylesExtractor {
                 writeDash(this.dash, this.edges);
                 writeLineWidth(this.lineWidth, this.edges);
                 writeEdgeColor(this.foreground, this.edges);
+                writeEdgePostaction(this.look, this.edges);
             }
         }
 
@@ -578,6 +627,22 @@ public final class TikzStylesExtractor {
             int b = color.getBlue();
             // {r,g,b}
             return "{" + r + "," + g + "," + b + "}";
+        }
+
+        // EZ say: We use postactions to draw inner lines. So far, only the ADDER style (used in
+        // cnew: elements) has an inner line, so I just created this special case here.
+        // If some new styles popup with similar needs then we need to generalize the code.
+        static void writeNodePostaction(Look look, List<StyleDuo> styles) {
+            if (look == ADDER) {
+                styles.add(new StyleDuo("postaction", "{draw=creator_c,thick,solid}"));
+            }
+        }
+
+        static void writeEdgePostaction(Look look, List<StyleDuo> styles) {
+            if (look == ADDER) {
+                styles
+                    .add(new StyleDuo("postaction", "{draw=creator_c,thick,solid,shorten >=3pt}"));
+            }
         }
     }
 
