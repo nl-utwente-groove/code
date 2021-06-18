5d039a774c3235a646d7d13bc602cb38814f8c24
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2021-06-18 21:33:26 +0000

    Adaptation of test suite to parse new operators

--- groove/trunk/src/groove/grammar/aspect/GraphConverter.java
+++ groove/trunk/src/groove/grammar/aspect/GraphConverter.java
@@ -1,15 +1,15 @@
 /* GROOVE: GRaphs for Object Oriented VErification
  * Copyright 2003--2011 University of Twente
  *
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
- * You may obtain a copy of the License at 
- * http://www.apache.org/licenses/LICENSE-2.0 
- * 
- * Unless required by applicable law or agreed to in writing, 
- * software distributed under the License is distributed on an 
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
- * either express or implied. See the License for the specific 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
  * language governing permissions and limitations under the License.
  *
  * $Id$
@@ -24,6 +24,10 @@ import static groove.grammar.aspect.AspectKind.SUBTYPE;
 import static groove.grammar.aspect.AspectKind.ContentKind.MULTIPLICITY;
 import static groove.graph.GraphRole.HOST;
 import static groove.graph.GraphRole.TYPE;
+
+import java.util.Map;
+import java.util.Set;
+
 import groove.grammar.host.HostEdge;
 import groove.grammar.host.HostGraph;
 import groove.grammar.host.HostNode;
@@ -54,10 +58,10 @@ public class GraphConverter {
         return result;
     }
 
-    /** 
+    /**
      * Converts a type graph to an aspect graph.
      * @return the resulting aspect graph, together with an element map
-     * from the type graph to the aspect graph. 
+     * from the type graph to the aspect graph.
      */
     static public TypeToAspectMap toAspectMap(TypeGraph type) {
         AspectGraph target = new AspectGraph(type.getName(), TYPE);
@@ -65,21 +69,29 @@ public class GraphConverter {
         for (TypeNode node : type.nodeSet()) {
             AspectNode nodeImage = target.addNode(node.getNumber());
             result.putNode(node, nodeImage);
-            target.addEdge(nodeImage, node.label().toParsableString(), nodeImage);
+            target.addEdge(nodeImage,
+                node.label()
+                    .toParsableString(),
+                nodeImage);
             if (node.isAbstract()) {
                 target.addEdge(nodeImage, ABSTRACT.getPrefix(), nodeImage);
             }
         }
+        Map<TypeNode,Set<TypeNode>> superMap = type.getDirectSupertypeMap();
         // add subtype relations
         for (TypeNode node : type.nodeSet()) {
             AspectNode nodeImage = result.getNode(node);
-            for (TypeNode nodeSuper : node.getSupertypes()) {
+            for (TypeNode nodeSuper : superMap.get(node)) {
                 target.addEdge(nodeImage, SUBTYPE.getPrefix(), result.getNode(nodeSuper));
             }
         }
         // add type edges
         for (TypeEdge edge : type.edgeSet()) {
             StringBuilder text = new StringBuilder();
+            if (edge.label()
+                .getRole() == EdgeRole.FLAG) {
+                text.append(EdgeRole.FLAG.getPrefix());
+            }
             if (edge.isAbstract()) {
                 text.append(ABSTRACT.getPrefix());
             }
@@ -93,9 +105,9 @@ public class GraphConverter {
                 text.append(MULTIPLICITY.toString(MULT_OUT, edge.getOutMult()));
             }
             text.append(edge.text());
-            AspectEdge edgeImage =
-                target.addEdge(result.getNode(edge.source()), text.toString(),
-                    result.getNode(edge.target()));
+            AspectEdge edgeImage = target.addEdge(result.getNode(edge.source()),
+                text.toString(),
+                result.getNode(edge.target()));
             result.putEdge(edge, edgeImage);
         }
         GraphInfo.transfer(type, target, result);
@@ -103,10 +115,10 @@ public class GraphConverter {
         return result;
     }
 
-    /** 
+    /**
      * Converts a host graph to an aspect graph.
      * @return the resulting aspect graph, together with an element map
-     * from the host graph to the aspect graph. 
+     * from the host graph to the aspect graph.
      */
     static public HostToAspectMap toAspectMap(HostGraph host) {
         AspectGraph targetGraph = new AspectGraph(host.getName(), HOST);
@@ -115,7 +127,8 @@ public class GraphConverter {
             if (!(node instanceof ValueNode)) {
                 AspectNode nodeImage = targetGraph.addNode(node.getNumber());
                 result.putNode(node, nodeImage);
-                TypeLabel typeLabel = node.getType().label();
+                TypeLabel typeLabel = node.getType()
+                    .label();
                 if (typeLabel != TypeLabel.NODE) {
                     targetGraph.addEdge(nodeImage, result.mapLabel(typeLabel), nodeImage);
                 }
@@ -123,27 +136,32 @@ public class GraphConverter {
         }
         // add edge images
         for (HostEdge edge : host.edgeSet()) {
-            String edgeText = edge.label().text();
+            String edgeText = edge.label()
+                .text();
             AspectNode imageSource = result.getNode(edge.source());
             AspectNode imageTarget;
             String text;
             if (edge.target() instanceof ValueNode) {
                 imageTarget = imageSource;
-                String constant = ((ValueNode) edge.target()).getTerm().toParseString();
+                String constant = ((ValueNode) edge.target()).getTerm()
+                    .toParseString();
                 text = AspectKind.LET.getPrefix() + edgeText + "=" + constant;
             } else if (edge.getRole() == EdgeRole.BINARY) {
                 imageTarget = result.getNode(edge.target());
                 // precede with literal aspect prefix if this is necessary
                 // to parse the label
-                AspectLabel tryLabel = AspectParser.getInstance().parse(edgeText, HOST);
-                if (tryLabel.hasErrors() || !tryLabel.getInnerText().equals(edgeText)) {
+                AspectLabel tryLabel = AspectParser.getInstance()
+                    .parse(edgeText, HOST);
+                if (tryLabel.hasErrors() || !tryLabel.getInnerText()
+                    .equals(edgeText)) {
                     text = AspectKind.LITERAL.getPrefix() + edgeText;
                 } else {
                     text = edgeText;
                 }
             } else {
                 imageTarget = imageSource;
-                text = edge.label().toString();
+                text = edge.label()
+                    .toString();
             }
             AspectEdge edgeImage = targetGraph.addEdge(imageSource, text, imageTarget);
             result.putEdge(edge, edgeImage);
@@ -153,12 +171,12 @@ public class GraphConverter {
         return result;
     }
 
-    /** 
+    /**
      * Mapping from the elements of a host graph to those of a corresponding
-     * aspect graph. For convenience, the aspect graph is bundled in with the map.  
+     * aspect graph. For convenience, the aspect graph is bundled in with the map.
      */
-    static public class HostToAspectMap extends
-            AElementMap<HostNode,HostEdge,AspectNode,AspectEdge> {
+    static public class HostToAspectMap
+        extends AElementMap<HostNode,HostEdge,AspectNode,AspectEdge> {
         /**
          * Creates a new, empty map.
          */
@@ -175,12 +193,12 @@ public class GraphConverter {
         private final AspectGraph aspectGraph;
     }
 
-    /** 
+    /**
      * Mapping from the elements of a type graph to those of a corresponding
-     * aspect graph. For convenience, the aspect graph is bundled in with the map.  
+     * aspect graph. For convenience, the aspect graph is bundled in with the map.
      */
-    static public class TypeToAspectMap extends
-            AElementMap<TypeNode,TypeEdge,AspectNode,AspectEdge> {
+    static public class TypeToAspectMap
+        extends AElementMap<TypeNode,TypeEdge,AspectNode,AspectEdge> {
         /**
          * Creates a new, empty map.
          */
