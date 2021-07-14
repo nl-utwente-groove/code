e75cfcc72691aa37328ae1c6e99620bfdb3d9400
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2021-07-14 18:23:50 +0000

    - Added nl.utwente to package names - Transferred dependencies to maven

---
+++ groove/branches/maven/src/main/java/nl/utwente/groove/gui/look/AspectValue.java
@@ -0,0 +1,48 @@
+/* GROOVE: GRaphs for Object Oriented VErification
+ * Copyright 2003--2011 University of Twente
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ * $Id$
+ */
+package nl.utwente.groove.gui.look;
+
+import nl.utwente.groove.graph.Graph;
+import nl.utwente.groove.gui.jgraph.AspectJEdge;
+import nl.utwente.groove.gui.jgraph.AspectJVertex;
+import nl.utwente.groove.gui.jgraph.JCell;
+import nl.utwente.groove.gui.jgraph.JGraph;
+
+/**
+ * Visual value strategy that delegates its task to
+ * specialised helper methods.
+ * @author Arend Rensink
+ * @version $Revision $
+ */
+public abstract class AspectValue<T> implements VisualValue<T> {
+    @Override
+    public <G extends Graph> T get(JGraph<G> jGraph, JCell<G> cell) {
+        if (cell instanceof AspectJVertex) {
+            return getForJVertex((AspectJVertex) cell);
+        }
+        if (cell instanceof AspectJEdge) {
+            return getForJEdge((AspectJEdge) cell);
+        }
+        return null;
+    }
+
+    /** Delegate method to retrieve the visual value from an {@link AspectJVertex}. */
+    abstract protected T getForJVertex(AspectJVertex jVertex);
+
+    /** Delegate method to retrieve the visual value from an {@link AspectJEdge}. */
+    abstract protected T getForJEdge(AspectJEdge jEdge);
+}
---
+++ groove/branches/maven/src/test/java/nl/utwente/groove/test/graph/NodeSetEdgeSetGraphTest.java
@@ -0,0 +1,40 @@
+// GROOVE: GRaphs for Object Oriented VErification
+// Copyright 2003--2007 University of Twente
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+// either express or implied. See the License for the specific
+// language governing permissions and limitations under the License.
+/*
+ * $Id$
+ */
+package nl.utwente.groove.test.graph;
+
+import nl.utwente.groove.graph.ElementFactory;
+import nl.utwente.groove.graph.NodeSetEdgeSetGraph;
+import nl.utwente.groove.graph.plain.PlainEdge;
+import nl.utwente.groove.graph.plain.PlainFactory;
+import nl.utwente.groove.graph.plain.PlainNode;
+
+/**
+ * Test class to test <tt>DefaultGraph</tt>
+ * @author Arend Rensink
+ * @version $Revision$
+ */
+public class NodeSetEdgeSetGraphTest extends GraphTest {
+    @Override
+    NodeSetEdgeSetGraph<PlainNode,PlainEdge> createGraph(String name) {
+        return new NodeSetEdgeSetGraph<PlainNode,PlainEdge>(name) {
+            @Override
+            public ElementFactory<PlainNode,PlainEdge> getFactory() {
+                return PlainFactory.instance();
+            }
+        };
+    }
+}
