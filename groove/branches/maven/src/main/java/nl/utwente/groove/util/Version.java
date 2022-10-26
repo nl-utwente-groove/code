9b4cf07e03f852be5062d285bd8fac8b918e8c1f
rensink <rensink@ffc74940-d049-0410-a542-8d04d3d4ca3b>
2022-10-26 15:34:12 +0000

    - Removed all test suites (they don't run well in a modular world) - Created launch configurations - Updated INSTALL instructions - Added Eclipse + Maven problem file - Set version to 6.0.0

--- groove/branches/maven/src/main/java/nl/utwente/groove/util/Version.java
+++ groove/branches/maven/src/main/java/nl/utwente/groove/util/Version.java
@@ -61,7 +61,7 @@ public class Version {
     }
 
     /** Build number (timestamp with format yyyyMMddHHmmssSSS). */
-    public static final String BUILD = "20210618";
+    public static final String BUILD = "20221026";
 
     /** Release date of this version (date format dd.MM.yyyy). */
     public static final String DATE;
@@ -82,10 +82,10 @@ public class Version {
      * </ul>
      * A '+' sign at the end of the number indicates a development version.
      */
-    public static final String NUMBER = "5.8.0+";
+    public static final String NUMBER = "6.0.0";
 
     /** Minimum Java JRE version required. */
-    static public final String NUMBER_JAVAMIN = "1.8";
+    static public final String NUMBER_JAVAMIN = "16";
 
     /** Title of this project. */
     static public final String TITLE = "GROOVE";
