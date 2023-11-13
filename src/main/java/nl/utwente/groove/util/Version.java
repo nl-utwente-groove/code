296301d1a94e84ebe702c0370ca385d0532efa14
Arend Rensink <arend.rensink@utwente.nl>
2023-11-13 14:38:55 +0000

    Preparing release 6.6.0

--- src/main/java/nl/utwente/groove/util/Version.java
+++ src/main/java/nl/utwente/groove/util/Version.java
@@ -82,7 +82,7 @@ public class Version {
      * </ul>
      * A '+' sign at the end of the number indicates a development version.
      */
-    public static final String NUMBER = "6.5.2-SNAPSHOT";
+    public static final String NUMBER = "6.6.0-SNAPSHOT";
 
     /** Minimum Java JRE version required. */
     static public final String NUMBER_JAVAMIN = "17";
