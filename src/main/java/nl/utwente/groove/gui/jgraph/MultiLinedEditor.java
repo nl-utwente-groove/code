7737c3d6ba7f282f617df11cc21c00b7a06fb449
Arend Rensink <arend.rensink@utwente.nl>
2026-09-12 10:15:05 +0200

    Fixed four javadoc warnings of the release build
    Cherry-picked from 8096efd70 on the yFiles migration line, minus its fifth
    hunk, which fixed a link in gui/view/InterpolatingBezier — a class that only
    exists on that line.

--- src/main/java/nl/utwente/groove/gui/jgraph/MultiLinedEditor.java
+++ src/main/java/nl/utwente/groove/gui/jgraph/MultiLinedEditor.java
@@ -80,7 +80,7 @@ public class MultiLinedEditor extends DefaultGraphCellEditor {
 
     /**
      * Overwriting this so that I could modify an editor container. see
-     * http://sourceforge.net/forum/forum.php?thread_id=781479&forum_id=140880
+     * {@code http://sourceforge.net/forum/forum.php?thread_id=781479&forum_id=140880}
      */
     @Override
     protected Container createContainer() {
